d07e8b24fd20202f578e367533221586c585a7e5
ProjectRevoTPP <projectrevotpp@hotmail.com>
2018-01-07 23:47:27 -0500

    NDEBUG which is supposed to be NDEBUG...

--- include/config.h
+++ include/config.h
@@ -4,9 +4,8 @@
 // In the Generation 3 games, Asserts were used in various debug builds.
 // Ruby/Sapphire and Emerald do not have these asserts while Fire Red
 // still has them in the ROM. This is because the developers forgot
-// to define NOAGBPRN before release, which is actually supposed to be
-// NDEBUG, however this has been changed as Ruby's actual debug build
-// does not use the AGBPrint features.
+// to define NDEBUG before release, however this has been changed as
+// Ruby's actual debug build does not use the AGBPrint features.
 // #define NDEBUG
 
 // Fire Red likely forgot to define NDEBUG/NOAGBPRN before release, leading

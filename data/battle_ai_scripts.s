6ddf9cf6305bc6ab094ddcd5fe402f121570b55b
aplumafreak500 <aplumafreak500@gmail.com>
2018-03-20 13:22:13 -0400

    battle_ai_scripts - first table

--- data/battle_ai_scripts.s
+++ data/battle_ai_scripts.s
@@ -1,4 +1,71 @@
+	.include asm/macros/battle_ai_script.inc
 	.section script_data, "aw", %progbits
 
 gUnknown_81D9BF4:: @ 81D9BF4
-	.incbin "data/battle_ai_scripts.bin"
+	.4byte gUnknown_81D9C74
+	.4byte gUnknown_81DA445
+	.4byte gUnknown_81DBA6F
+	.4byte gUnknown_81DBA8D
+	.4byte gUnknown_81DBAEF
+	.4byte gUnknown_81DBADF
+	.4byte gUnknown_81DBB16
+	.4byte gUnknown_81DBB3B
+	.4byte gUnknown_81DBB3C
+	.4byte_gUnknown_81DBC91
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCED
+	.4byte gUnknown_81DBCA8
+	.4byte gUnknown_81DBCD6
+	.4byte gUnknown_81DBCDD
+
+gUnknown_81D9C74::
+	.incbin "baserom.gba", 0x1D9C74, 0x7D1
+
+gUnknown_81DA445::
+	.incbin "baserom.gba", 0x1DA445, 0x2A
+
+gUnknown_81DBA6F::
+	.incbin "baserom.gba", 0x1DBA6F, 0x1E
+
+gUnknown_81DBA8D::
+	.incbin "baserom.gba", 0x1DBA8D, 0x52
+
+gUnknown_81DA4EF::
+	.incbin "baserom.gba", 0x1DA4EF, 0x1627
+
+gUnknown_81DA4DF::
+	.incbin "baserom.gba", 0x1DA4DF, 0x10
+
+gUnknown_81DBB16::
+	.incbin "baserom.gba", 0x1DBB16, 0x25
+
+gUnknown_81DBB3B::
+	.byte 0x5A
+
+gUnknown_81DBB3C::
+	.incbin "baserom.gba", 0x1DBB3C, 0x155
+
+gUnknown_81DBC91::
+	.incbin "baserom.gba", 0x1DBC91, 0x17
+
+gUnknown_81DBCA8::
+	.incbin "baserom.gba", 0x1DBCA8, 0x2E
+
+gUnknown_81DBCD6::
+	.incbin "baserom.gba", 0x1DBCD6, 0x17
+
+gUnknown_81DBCED::
+	.byte 0x5A
+

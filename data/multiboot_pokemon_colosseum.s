5afecafc31a9ed9821fd8459f8521a6ae5a4d0ec
aplumafreak500 <aplumafreak500@gmail.com>
2018-04-04 15:09:53 -0400

    typo

--- data/multiboot_pokemon_colosseum.s
+++ data/multiboot_pokemon_colosseum.s
@@ -1,5 +1,5 @@
 	.section .rodata
 
 gMultiBootProgram_PokemonColosseum_Start:: @ 8703860
-	.incbin "multiboot_pokemon_colosseum.bin"
+	.incbin "data/multiboot_pokemon_colosseum.bin"
 gMultiBootProgram_PokemonColosseum_End::

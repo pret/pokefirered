fcc77d3b96ef6f8c314f6427b4f0c17ce66ac6f7
PikalaxALT <pikalaxalt@gmail.com>
2019-04-12 13:38:48 -0400

    split off field_weather data

---
+++ data/title_screen.s
@@ -0,0 +1,34 @@
+	.section .rodata
+	.align 2
+gUnknown_83BF58C:: @ 83BF58C
+	.incbin "baserom.gba", 0x3BF58C, 0x1C
+
+gUnknown_83BF5A8:: @ 83BF5A8
+	.incbin "baserom.gba", 0x3BF5A8, 0x55C
+
+gUnknown_83BFB04:: @ 83BFB04
+	.incbin "baserom.gba", 0x3BFB04, 0x18
+
+gUnknown_83BFB1C:: @ 83BFB1C
+	.incbin "baserom.gba", 0x3BFB1C, 0x20
+
+gUnknown_83BFB3C:: @ 83BFB3C
+	.incbin "baserom.gba", 0x3BFB3C, 0x20
+
+gUnknown_83BFB5C:: @ 83BFB5C
+	.incbin "baserom.gba", 0x3BFB5C, 0x18
+
+gUnknown_83BFB74:: @ 83BFB74
+	.incbin "baserom.gba", 0x3BFB74, 0x10
+
+gUnknown_83BFB84:: @ 83BFB84
+	.incbin "baserom.gba", 0x3BFB84, 0x18
+
+gUnknown_83BFB9C:: @ 83BFB9C
+	.incbin "baserom.gba", 0x3BFB9C, 0x20
+
+gUnknown_83BFBBC:: @ 83BFBBC
+	.incbin "baserom.gba", 0x3BFBBC, 0x18
+
+gUnknown_83BFBD4:: @ 83BFBD4
+	.incbin "baserom.gba", 0x3BFBD4, 0x2FE8

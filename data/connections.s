8ec229b5570acf7b1efd6192bc27968eafdc0474
PikalaxALT <pikalaxalt@gmail.com>
2019-04-13 14:12:36 -0400

    Initial map header dump

--- data/connections.s
+++ data/connections.s
@@ -7,7 +7,7 @@ gUnknown_8352754:: @ 8352754
 	connection up, 0, MAP_ROUTE1
 	connection down, 0, MAP_ROUTE21_NORTH
 
-gUnknown_835276C:: @ 835276C
+gMapConnections_PalletTown:: @ 835276C
 	.4byte 2
 	.4byte gUnknown_8352754
 
@@ -16,7 +16,7 @@ gUnknown_8352774:: @ 8352774
 	connection down, 12, MAP_ROUTE1
 	connection left, 10, MAP_ROUTE22
 
-gUnknown_8352798:: @ 8352798
+gMapConnections_ViridianCity:: @ 8352798
 	.4byte 3
 	.4byte gUnknown_8352774
 
@@ -24,7 +24,7 @@ gUnknown_83527A0:: @ 83527A0
 	connection down, 12, MAP_ROUTE2
 	connection right, 10, MAP_ROUTE3
 
-gUnknown_83527B8:: @ 83527B8
+gMapConnections_PewterCity:: @ 83527B8
 	.4byte 2
 	.4byte gUnknown_83527A0
 
@@ -34,7 +34,7 @@ gUnknown_83527C0:: @ 83527C0
 	connection left, 10, MAP_ROUTE4
 	connection right, 10, MAP_ROUTE9
 
-gUnknown_83527F0:: @ 83527F0
+gMapConnections_CeruleanCity:: @ 83527F0
 	.4byte 4
 	.4byte gUnknown_83527C0
 
@@ -43,7 +43,7 @@ gUnknown_83527F8:: @ 83527F8
 	connection down, 0, MAP_ROUTE12
 	connection left, 0, MAP_ROUTE8
 
-gUnknown_835281C:: @ 835281C
+gMapConnections_LavenderTown:: @ 835281C
 	.4byte 3
 	.4byte gUnknown_83527F8
 
@@ -51,7 +51,7 @@ gUnknown_8352824:: @ 8352824
 	connection up, 12, MAP_ROUTE6
 	connection right, 10, MAP_ROUTE11
 
-gUnknown_835283C:: @ 835283C
+gMapConnections_VermilionCity:: @ 835283C
 	.4byte 2
 	.4byte gUnknown_8352824
 
@@ -59,7 +59,7 @@ gUnknown_8352844:: @ 8352844
 	connection left, 10, MAP_ROUTE16
 	connection right, 10, MAP_ROUTE7
 
-gUnknown_835285C:: @ 835285C
+gMapConnections_CeladonCity:: @ 835285C
 	.4byte 2
 	.4byte gUnknown_8352844
 
@@ -68,7 +68,7 @@ gUnknown_8352864:: @ 8352864
 	connection left, 10, MAP_ROUTE18
 	connection right, 10, MAP_ROUTE15
 
-gUnknown_8352888:: @ 8352888
+gMapConnections_FuchsiaCity:: @ 8352888
 	.4byte 3
 	.4byte gUnknown_8352864
 
@@ -76,14 +76,14 @@ gUnknown_8352890:: @ 8352890
 	connection up, 0, MAP_ROUTE21_SOUTH
 	connection right, 0, MAP_ROUTE20
 
-gUnknown_83528A8:: @ 83528A8
+gMapConnections_CinnabarIsland:: @ 83528A8
 	.4byte 2
 	.4byte gUnknown_8352890
 
 gUnknown_83528B0:: @ 83528B0
 	connection down, 0, MAP_ROUTE23
 
-gUnknown_83528BC:: @ 83528BC
+gMapConnections_IndigoPlateau_Exterior:: @ 83528BC
 	.4byte 1
 	.4byte gUnknown_83528B0
 
@@ -103,7 +103,7 @@ gUnknown_83528FC:: @ 83528FC
 	connection left, 10, MAP_ROUTE7
 	connection right, 10, MAP_ROUTE8
 
-gUnknown_835292C:: @ 835292C
+gMapConnections_SaffronCity:: @ 835292C
 	.4byte 4
 	.4byte gUnknown_83528FC
 
@@ -111,14 +111,14 @@ gUnknown_8352934:: @ 8352934
 	connection down, 0, MAP_ONEISLAND_TREASUREBEACH
 	connection right, -120, MAP_ONEISLAND_KINDLEROAD
 
-gUnknown_835294C:: @ 835294C
+gMapConnections_OneIsland:: @ 835294C
 	.4byte 2
 	.4byte gUnknown_8352934
 
 gUnknown_8352954:: @ 8352954
 	connection up, 24, MAP_TWOISLAND_CAPEBRINK
 
-gUnknown_8352960:: @ 8352960
+gMapConnections_TwoIsland:: @ 8352960
 	.4byte 1
 	.4byte gUnknown_8352954
 
@@ -126,7 +126,7 @@ gUnknown_8352968:: @ 8352968
 	connection down, 0, MAP_THREEISLAND_PORT
 	connection left, 0, MAP_THREEISLAND_BONDBRIDGE
 
-gUnknown_8352980:: @ 8352980
+gMapConnections_ThreeIsland:: @ 8352980
 	.4byte 2
 	.4byte gUnknown_8352968
 
@@ -134,7 +134,7 @@ gUnknown_8352988:: @ 8352988
 	connection up, -48, MAP_FIVEISLAND_WATERLABYRINTH
 	connection right, 0, MAP_FIVEISLAND_MEADOW
 
-gUnknown_83529A0:: @ 83529A0
+gMapConnections_FiveIsland:: @ 83529A0
 	.4byte 2
 	.4byte gUnknown_8352988
 
@@ -142,14 +142,14 @@ gUnknown_83529A8:: @ 83529A8
 	connection up, -48, MAP_SEVENISLAND_TRAINERTOWER
 	connection down, 0, MAP_SEVENISLAND_SEVAULTCANYON_ENTRANCE
 
-gUnknown_83529C0:: @ 83529C0
+gMapConnections_SevenIsland:: @ 83529C0
 	.4byte 2
 	.4byte gUnknown_83529A8
 
 gUnknown_83529C8:: @ 83529C8
 	connection right, -40, MAP_SIXISLAND_WATERPATH
 
-gUnknown_83529D4:: @ 83529D4
+gMapConnections_SixIsland:: @ 83529D4
 	.4byte 1
 	.4byte gUnknown_83529C8
 
@@ -157,7 +157,7 @@ gUnknown_83529DC:: @ 83529DC
 	connection up, -12, MAP_VIRIDIANCITY
 	connection down, 0, MAP_PALLETTOWN
 
-gUnknown_83529F4:: @ 83529F4
+gMapConnections_Route1:: @ 83529F4
 	.4byte 2
 	.4byte gUnknown_83529DC
 
@@ -165,7 +165,7 @@ gUnknown_83529FC:: @ 83529FC
 	connection up, -12, MAP_PEWTERCITY
 	connection down, -12, MAP_VIRIDIANCITY
 
-gUnknown_8352A14:: @ 8352A14
+gMapConnections_Route2:: @ 8352A14
 	.4byte 2
 	.4byte gUnknown_83529FC
 
@@ -173,7 +173,7 @@ gUnknown_8352A1C:: @ 8352A1C
 	connection up, 60, MAP_ROUTE4
 	connection left, -10, MAP_PEWTERCITY
 
-gUnknown_8352A34:: @ 8352A34
+gMapConnections_Route3:: @ 8352A34
 	.4byte 2
 	.4byte gUnknown_8352A1C
 
@@ -181,7 +181,7 @@ gUnknown_8352A3C:: @ 8352A3C
 	connection down, -60, MAP_ROUTE3
 	connection right, -10, MAP_CERULEANCITY
 
-gUnknown_8352A54:: @ 8352A54
+gMapConnections_Route4:: @ 8352A54
 	.4byte 2
 	.4byte gUnknown_8352A3C
 
@@ -189,7 +189,7 @@ gUnknown_8352A5C:: @ 8352A5C
 	connection up, 0, MAP_CERULEANCITY
 	connection down, 0, MAP_SAFFRONCITY
 
-gUnknown_8352A74:: @ 8352A74
+gMapConnections_Route5:: @ 8352A74
 	.4byte 2
 	.4byte gUnknown_8352A5C
 
@@ -197,7 +197,7 @@ gUnknown_8352A7C:: @ 8352A7C
 	connection up, 0, MAP_SAFFRONCITY
 	connection down, -12, MAP_VERMILIONCITY
 
-gUnknown_8352A94:: @ 8352A94
+gMapConnections_Route6:: @ 8352A94
 	.4byte 2
 	.4byte gUnknown_8352A7C
 
@@ -205,7 +205,7 @@ gUnknown_8352A9C:: @ 8352A9C
 	connection left, -10, MAP_CELADONCITY
 	connection right, -10, MAP_SAFFRONCITY
 
-gUnknown_8352AB4:: @ 8352AB4
+gMapConnections_Route7:: @ 8352AB4
 	.4byte 2
 	.4byte gUnknown_8352A9C
 
@@ -213,7 +213,7 @@ gUnknown_8352ABC:: @ 8352ABC
 	connection left, -10, MAP_SAFFRONCITY
 	connection right, 0, MAP_LAVENDERTOWN
 
-gUnknown_8352AD4:: @ 8352AD4
+gMapConnections_Route8:: @ 8352AD4
 	.4byte 2
 	.4byte gUnknown_8352ABC
 
@@ -221,7 +221,7 @@ gUnknown_8352ADC:: @ 8352ADC
 	connection left, -10, MAP_CERULEANCITY
 	connection right, 0, MAP_ROUTE10
 
-gUnknown_8352AF4:: @ 8352AF4
+gMapConnections_Route9:: @ 8352AF4
 	.4byte 2
 	.4byte gUnknown_8352ADC
 
@@ -229,7 +229,7 @@ gUnknown_8352AFC:: @ 8352AFC
 	connection down, 0, MAP_LAVENDERTOWN
 	connection left, 0, MAP_ROUTE9
 
-gUnknown_8352B14:: @ 8352B14
+gMapConnections_Route10:: @ 8352B14
 	.4byte 2
 	.4byte gUnknown_8352AFC
 
@@ -237,7 +237,7 @@ gUnknown_8352B1C:: @ 8352B1C
 	connection left, -10, MAP_VERMILIONCITY
 	connection right, -60, MAP_ROUTE12
 
-gUnknown_8352B34:: @ 8352B34
+gMapConnections_Route11:: @ 8352B34
 	.4byte 2
 	.4byte gUnknown_8352B1C
 
@@ -246,7 +246,7 @@ gUnknown_8352B3C:: @ 8352B3C
 	connection down, -48, MAP_ROUTE13
 	connection left, 60, MAP_ROUTE11
 
-gUnknown_8352B60:: @ 8352B60
+gMapConnections_Route12:: @ 8352B60
 	.4byte 3
 	.4byte gUnknown_8352B3C
 
@@ -254,7 +254,7 @@ gUnknown_8352B68:: @ 8352B68
 	connection up, 48, MAP_ROUTE12
 	connection left, 0, MAP_ROUTE14
 
-gUnknown_8352B80:: @ 8352B80
+gMapConnections_Route13:: @ 8352B80
 	.4byte 2
 	.4byte gUnknown_8352B68
 
@@ -262,7 +262,7 @@ gUnknown_8352B88:: @ 8352B88
 	connection left, 40, MAP_ROUTE15
 	connection right, 0, MAP_ROUTE13
 
-gUnknown_8352BA0:: @ 8352BA0
+gMapConnections_Route14:: @ 8352BA0
 	.4byte 2
 	.4byte gUnknown_8352B88
 
@@ -270,7 +270,7 @@ gUnknown_8352BA8:: @ 8352BA8
 	connection left, -10, MAP_FUCHSIACITY
 	connection right, -40, MAP_ROUTE14
 
-gUnknown_8352BC0:: @ 8352BC0
+gMapConnections_Route15:: @ 8352BC0
 	.4byte 2
 	.4byte gUnknown_8352BA8
 
@@ -278,7 +278,7 @@ gUnknown_8352BC8:: @ 8352BC8
 	connection down, 0, MAP_ROUTE17
 	connection right, -10, MAP_CELADONCITY
 
-gUnknown_8352BE0:: @ 8352BE0
+gMapConnections_Route16:: @ 8352BE0
 	.4byte 2
 	.4byte gUnknown_8352BC8
 
@@ -286,7 +286,7 @@ gUnknown_8352BE8:: @ 8352BE8
 	connection up, 0, MAP_ROUTE16
 	connection down, 0, MAP_ROUTE18
 
-gUnknown_8352C00:: @ 8352C00
+gMapConnections_Route17:: @ 8352C00
 	.4byte 2
 	.4byte gUnknown_8352BE8
 
@@ -294,7 +294,7 @@ gUnknown_8352C08:: @ 8352C08
 	connection up, 0, MAP_ROUTE17
 	connection right, -10, MAP_FUCHSIACITY
 
-gUnknown_8352C20:: @ 8352C20
+gMapConnections_Route18:: @ 8352C20
 	.4byte 2
 	.4byte gUnknown_8352C08
 
@@ -302,7 +302,7 @@ gUnknown_8352C28:: @ 8352C28
 	connection up, -12, MAP_FUCHSIACITY
 	connection left, 40, MAP_ROUTE20
 
-gUnknown_8352C40:: @ 8352C40
+gMapConnections_Route19:: @ 8352C40
 	.4byte 2
 	.4byte gUnknown_8352C28
 
@@ -310,7 +310,7 @@ gUnknown_8352C48:: @ 8352C48
 	connection left, 0, MAP_CINNABARISLAND
 	connection right, -40, MAP_ROUTE19
 
-gUnknown_8352C60:: @ 8352C60
+gMapConnections_Route20:: @ 8352C60
 	.4byte 2
 	.4byte gUnknown_8352C48
 
@@ -318,7 +318,7 @@ gUnknown_8352C68:: @ 8352C68
 	connection up, 0, MAP_PALLETTOWN
 	connection down, 0, MAP_ROUTE21_SOUTH
 
-gUnknown_8352C80:: @ 8352C80
+gMapConnections_Route21_North:: @ 8352C80
 	.4byte 2
 	.4byte gUnknown_8352C68
 
@@ -326,7 +326,7 @@ gUnknown_8352C88:: @ 8352C88
 	connection up, 0, MAP_ROUTE21_NORTH
 	connection down, 0, MAP_CINNABARISLAND
 
-gUnknown_8352CA0:: @ 8352CA0
+gMapConnections_Route21_South:: @ 8352CA0
 	.4byte 2
 	.4byte gUnknown_8352C88
 
@@ -334,7 +334,7 @@ gUnknown_8352CA8:: @ 8352CA8
 	connection up, 0, MAP_ROUTE23
 	connection right, -10, MAP_VIRIDIANCITY
 
-gUnknown_8352CC0:: @ 8352CC0
+gMapConnections_Route22:: @ 8352CC0
 	.4byte 2
 	.4byte gUnknown_8352CA8
 
@@ -342,7 +342,7 @@ gUnknown_8352CC8:: @ 8352CC8
 	connection up, 0, MAP_INDIGOPLATEAU_EXTERIOR
 	connection down, 0, MAP_ROUTE22
 
-gUnknown_8352CE0:: @ 8352CE0
+gMapConnections_Route23:: @ 8352CE0
 	.4byte 2
 	.4byte gUnknown_8352CC8
 
@@ -350,70 +350,70 @@ gUnknown_8352CE8:: @ 8352CE8
 	connection down, -12, MAP_CERULEANCITY
 	connection right, 0, MAP_ROUTE25
 
-gUnknown_8352D00:: @ 8352D00
+gMapConnections_Route24:: @ 8352D00
 	.4byte 2
 	.4byte gUnknown_8352CE8
 
 gUnknown_8352D08:: @ 8352D08
 	connection left, 0, MAP_ROUTE24
 
-gUnknown_8352D14:: @ 8352D14
+gMapConnections_Route25:: @ 8352D14
 	.4byte 1
 	.4byte gUnknown_8352D08
 
 gUnknown_8352D1C:: @ 8352D1C
 	connection left, 120, MAP_ONEISLAND
 
-gUnknown_8352D28:: @ 8352D28
+gMapConnections_OneIsland_KindleRoad:: @ 8352D28
 	.4byte 1
 	.4byte gUnknown_8352D1C
 
 gUnknown_8352D30:: @ 8352D30
 	connection up, 0, MAP_ONEISLAND
 
-gUnknown_8352D3C:: @ 8352D3C
+gMapConnections_OneIsland_TreasureBeach:: @ 8352D3C
 	.4byte 1
 	.4byte gUnknown_8352D30
 
 gUnknown_8352D44:: @ 8352D44
 	connection down, -24, MAP_TWOISLAND
 
-gUnknown_8352D50:: @ 8352D50
+gMapConnections_TwoIsland_CapeBrink:: @ 8352D50
 	.4byte 1
 	.4byte gUnknown_8352D44
 
 gUnknown_8352D58:: @ 8352D58
 	connection right, 0, MAP_THREEISLAND
 
-gUnknown_8352D64:: @ 8352D64
+gMapConnections_ThreeIsland_BondBridge:: @ 8352D64
 	.4byte 1
 	.4byte gUnknown_8352D58
 
 gUnknown_8352D6C:: @ 8352D6C
 	connection up, 0, MAP_THREEISLAND
 
-gUnknown_8352D78:: @ 8352D78
+gMapConnections_ThreeIsland_Port:: @ 8352D78
 	.4byte 1
 	.4byte gUnknown_8352D6C
 
 gUnknown_8352D80:: @ 8352D80
 	connection up, 0, MAP_THREEISLAND
 
-gUnknown_8352D8C:: @ 8352D8C
+gMapConnections_UnknownMap_03_50:: @ 8352D8C
 	.4byte 1
 	.4byte gUnknown_8352D80
 
 gUnknown_8352D94:: @ 8352D94
 	connection up, 0, MAP_THREEISLAND
 
-gUnknown_8352DA0:: @ 8352DA0
+gMapConnections_UnknownMap_03_51:: @ 8352DA0
 	.4byte 1
 	.4byte gUnknown_8352D94
 
 gUnknown_8352DA8:: @ 8352DA8
 	connection down, -48, MAP_FIVEISLAND_WATERLABYRINTH
 
-gUnknown_8352DB4:: @ 8352DB4
+gMapConnections_FiveIsland_ResortGorgeous:: @ 8352DB4
 	.4byte 1
 	.4byte gUnknown_8352DA8
 
@@ -421,7 +421,7 @@ gUnknown_8352DBC:: @ 8352DBC
 	connection up, 48, MAP_FIVEISLAND_RESORTGORGEOUS
 	connection down, 48, MAP_FIVEISLAND
 
-gUnknown_8352DD4:: @ 8352DD4
+gMapConnections_FiveIsland_WaterLabyrinth:: @ 8352DD4
 	.4byte 2
 	.4byte gUnknown_8352DBC
 
@@ -429,21 +429,21 @@ gUnknown_8352DDC:: @ 8352DDC
 	connection left, 0, MAP_FIVEISLAND
 	connection right, 20, MAP_FIVEISLAND_MEMORIALPILLAR
 
-gUnknown_8352DF4:: @ 8352DF4
+gMapConnections_FiveIsland_Meadow:: @ 8352DF4
 	.4byte 2
 	.4byte gUnknown_8352DDC
 
 gUnknown_8352DFC:: @ 8352DFC
 	connection left, -20, MAP_FIVEISLAND_MEADOW
 
-gUnknown_8352E08:: @ 8352E08
+gMapConnections_FiveIsland_MemorialPillar:: @ 8352E08
 	.4byte 1
 	.4byte gUnknown_8352DFC
 
 gUnknown_8352E10:: @ 8352E10
 	connection down, 0, MAP_SIXISLAND_GREENPATH
 
-gUnknown_8352E1C:: @ 8352E1C
+gMapConnections_SixIsland_OutcastIsland:: @ 8352E1C
 	.4byte 1
 	.4byte gUnknown_8352E10
 
@@ -451,7 +451,7 @@ gUnknown_8352E24:: @ 8352E24
 	connection up, 0, MAP_SIXISLAND_OUTCASTISLAND
 	connection right, 0, MAP_SIXISLAND_WATERPATH
 
-gUnknown_8352E3C:: @ 8352E3C
+gMapConnections_SixIsland_GreenPath:: @ 8352E3C
 	.4byte 2
 	.4byte gUnknown_8352E24
 
@@ -460,21 +460,21 @@ gUnknown_8352E44:: @ 8352E44
 	connection left, 40, MAP_SIXISLAND
 	connection left, 80, MAP_SIXISLAND_RUINVALLEY
 
-gUnknown_8352E68:: @ 8352E68
+gMapConnections_SixIsland_WaterPath:: @ 8352E68
 	.4byte 3
 	.4byte gUnknown_8352E44
 
 gUnknown_8352E70:: @ 8352E70
 	connection right, -80, MAP_SIXISLAND_WATERPATH
 
-gUnknown_8352E7C:: @ 8352E7C
+gMapConnections_SixIsland_RuinValley:: @ 8352E7C
 	.4byte 1
 	.4byte gUnknown_8352E70
 
 gUnknown_8352E84:: @ 8352E84
 	connection down, 48, MAP_SEVENISLAND
 
-gUnknown_8352E90:: @ 8352E90
+gMapConnections_SevenIsland_TrainerTower:: @ 8352E90
 	.4byte 1
 	.4byte gUnknown_8352E84
 
@@ -482,7 +482,7 @@ gUnknown_8352E98:: @ 8352E98
 	connection up, 0, MAP_SEVENISLAND
 	connection right, 20, MAP_SEVENISLAND_SEVAULTCANYON
 
-gUnknown_8352EB0:: @ 8352EB0
+gMapConnections_SevenIsland_SevaultCanyon_Entrance:: @ 8352EB0
 	.4byte 2
 	.4byte gUnknown_8352E98
 
@@ -490,14 +490,13 @@ gUnknown_8352EB8:: @ 8352EB8
 	connection down, -48, MAP_SEVENISLAND_TANOBYRUINS
 	connection left, -20, MAP_SEVENISLAND_SEVAULTCANYON_ENTRANCE
 
-gUnknown_8352ED0:: @ 8352ED0
+gMapConnections_SevenIsland_SevaultCanyon:: @ 8352ED0
 	.4byte 2
 	.4byte gUnknown_8352EB8
 
 gUnknown_8352ED8:: @ 8352ED8
 	connection up, 48, MAP_SEVENISLAND_SEVAULTCANYON
 
-gUnknown_8352EE4:: @ 8352EE4
+gMapConnections_SevenIsland_TanobyRuins:: @ 8352EE4
 	.4byte 1
 	.4byte gUnknown_8352ED8
-

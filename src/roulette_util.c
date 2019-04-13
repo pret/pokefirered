9e1028f6f1883e0fb14736b2e6b99f2dfa78e51e
SatoMew <SatoMew@users.noreply.github.com>
2019-04-13 18:13:39 +0100

    Create roulette_util.h

--- src/roulette_util.c
+++ src/roulette_util.c
@@ -1,70 +1,7 @@
 #include "global.h"
 #include "palette.h"
+#include "roulette_util.h"
 #include "util.h"
-
-struct UnkStruct1
-{
-    u16 var00;
-    u16 var02;
-    u8 var04;
-    u8 var05;
-    u8 var06;
-    s8 var07_0:5;
-    s8 var07_5:2;
-    s8 var07_7:1;
-};
-
-struct UnkStruct3
-{
-    u8 var00_0:7;
-    u8 var00_7:1;
-    u8 var01;
-    s8 var02;
-    s8 var03;
-    struct UnkStruct1 var04;
-};
-
-struct UnkStruct0
-{
-    u8 var00;
-    u8 var01;
-    u16 var02; //flag for each UnkStruct3
-    struct UnkStruct3 var04[0x10];
-};
-
-struct PulseBlendSettings
-{
-    u16 blendColor;
-    u16 paletteOffset;
-    u8 numColors;
-    u8 delay;
-    u8 numFadeCycles;
-    s8 maxBlendCoeff:4;
-    s8 fadeType:2;
-    s8 restorePaletteOnUnload:1;
-    s8 unk7_7:1;
-};
-
-struct PulseBlendPalette
-{
-    u8 paletteSelector;
-    u8 blendCoeff:4;
-    u8 fadeDirection:1;
-    s8 unk1_5:1;
-    s8 available:1;
-    u32 inUse:1;
-    u8 delayCounter;
-    u8 fadeCycleCounter;
-    struct PulseBlendSettings pulseBlendSettings;
-};
-
-struct PulseBlend
-{
-    u16 usedPulseBlendPalettes;
-    struct PulseBlendPalette pulseBlendPalettes[16];
-};
-
-void BlendPalette(u16 palOffset, u16 numEntries, u8 coeff, u16 blendColor);
 
 void sub_80D8BE4(struct UnkStruct0 *r0)
 {

425c733ceabd6604b3ffda140fa0f97c58a53689
PikalaxALT <pikalaxalt@gmail.com>
2019-05-12 17:24:55 -0400

    split unk_8159F40

--- src/unk_8159F40.c
+++ src/unk_8159F40.c
@@ -620,22 +620,3 @@ void sub_815A540(void)
         }
     }
 }
-
-void sub_815A5BC(s32 a0)
-{
-    struct Padded_U8 data[2];
-    data[0].value = 1;
-    data[1].value = a0;
-    sub_80F9E2C(data);
-}
-
-u8 sub_815A5E8(s32 a0)
-{
-    u8 * r1;
-    if ((gRecvCmds[0][0] & 0xFF00) != 0x2F00)
-        return 0;
-    r1 = (u8 *)&gRecvCmds[a0][1];
-    if (r1[0] == 1)
-        return r1[4];
-    return 0;
-}

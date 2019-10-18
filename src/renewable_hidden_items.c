a7bca8aa92b7696047bcb3da8607ac7981647630
PikalaxALT <pikalaxalt@gmail.com>
2019-10-18 15:02:34 -0400

    Hidden item flags

--- src/renewable_hidden_items.c
+++ src/renewable_hidden_items.c
@@ -31,7 +31,7 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
             0xFF
         },
         .flags2 = {
-            FLAG_0x481 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_ROUTE20_STARDUST,
             0xFF,
             0xFF,
             0xFF,
@@ -65,7 +65,7 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
             0xFF
         },
         .flags2 = {
-            FLAG_0x482 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_ROUTE21_NORTH_PEARL,
             0xFF,
             0xFF,
             0xFF,
@@ -89,7 +89,7 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
         .mapGroup = MAP_GROUP(UNDERGROUND_PATH_NORTH_SOUTH_TUNNEL),
         .mapNum = MAP_NUM(UNDERGROUND_PATH_NORTH_SOUTH_TUNNEL),
         .flags1 = {
-            FLAG_0x434 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_UNDERGROUND_PATH_NORTH_SOUTH_TUNNEL_ETHER,
             0xFF,
             0xFF,
             0xFF,
@@ -99,12 +99,12 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
             0xFF
         },
         .flags2 = {
-            FLAG_0x42E - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x42F - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x430 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x431 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x432 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x433 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_UNDERGROUND_PATH_NORTH_SOUTH_TUNNEL_POTION,
+            HIDDEN_ITEM_UNDERGROUND_PATH_NORTH_SOUTH_TUNNEL_ANTIDOTE,
+            HIDDEN_ITEM_UNDERGROUND_PATH_NORTH_SOUTH_TUNNEL_PARALYZE_HEAL,
+            HIDDEN_ITEM_UNDERGROUND_PATH_NORTH_SOUTH_TUNNEL_AWAKENING,
+            HIDDEN_ITEM_UNDERGROUND_PATH_NORTH_SOUTH_TUNNEL_BURN_HEAL,
+            HIDDEN_ITEM_UNDERGROUND_PATH_NORTH_SOUTH_TUNNEL_ICE_HEAL,
             0xFF,
             0xFF
         },
@@ -123,7 +123,7 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
         .mapGroup = MAP_GROUP(UNDERGROUND_PATH_EAST_WEST_TUNNEL),
         .mapNum = MAP_NUM(UNDERGROUND_PATH_EAST_WEST_TUNNEL),
         .flags1 = {
-            FLAG_0x43B - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_UNDERGROUND_PATH_EAST_WEST_TUNNEL_ETHER,
             0xFF,
             0xFF,
             0xFF,
@@ -133,12 +133,12 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
             0xFF
         },
         .flags2 = {
-            FLAG_0x435 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x436 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x437 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x438 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x439 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x43A - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_UNDERGROUND_PATH_EAST_WEST_TUNNEL_POTION,
+            HIDDEN_ITEM_UNDERGROUND_PATH_EAST_WEST_TUNNEL_ANTIDOTE,
+            HIDDEN_ITEM_UNDERGROUND_PATH_EAST_WEST_TUNNEL_PARALYZE_HEAL,
+            HIDDEN_ITEM_UNDERGROUND_PATH_EAST_WEST_TUNNEL_AWAKENING,
+            HIDDEN_ITEM_UNDERGROUND_PATH_EAST_WEST_TUNNEL_BURN_HEAL,
+            HIDDEN_ITEM_UNDERGROUND_PATH_EAST_WEST_TUNNEL_ICE_HEAL,
             0xFF,
             0xFF
         },
@@ -157,10 +157,10 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
         .mapGroup = MAP_GROUP(SEVEN_ISLAND_TANOBY_RUINS),
         .mapNum = MAP_NUM(SEVEN_ISLAND_TANOBY_RUINS),
         .flags1 = {
-            FLAG_0x428 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x429 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x42A - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x42B - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_SEVEN_ISLAND_TANOBY_RUINS_HEART_SCALE_4,
+            HIDDEN_ITEM_SEVEN_ISLAND_TANOBY_RUINS_HEART_SCALE,
+            HIDDEN_ITEM_SEVEN_ISLAND_TANOBY_RUINS_HEART_SCALE_2,
+            HIDDEN_ITEM_SEVEN_ISLAND_TANOBY_RUINS_HEART_SCALE_3,
             0xFF,
             0xFF,
             0xFF,
@@ -191,19 +191,19 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
         .mapGroup = MAP_GROUP(MT_MOON_B1F),
         .mapNum = MAP_NUM(MT_MOON_B1F),
         .flags1 = {
-            FLAG_0x43C - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x43D - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x43E - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x43F - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x440 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x441 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_MT_MOON_B1F_TINY_MUSHROOM,
+            HIDDEN_ITEM_MT_MOON_B1F_TINY_MUSHROOM_2,
+            HIDDEN_ITEM_MT_MOON_B1F_TINY_MUSHROOM_3,
+            HIDDEN_ITEM_MT_MOON_B1F_BIG_MUSHROOM,
+            HIDDEN_ITEM_MT_MOON_B1F_BIG_MUSHROOM_2,
+            HIDDEN_ITEM_MT_MOON_B1F_BIG_MUSHROOM_3,
             0xFF,
             0xFF
         },
         .flags2 = {
-            FLAG_0x43C - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x43D - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x43E - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_MT_MOON_B1F_TINY_MUSHROOM,
+            HIDDEN_ITEM_MT_MOON_B1F_TINY_MUSHROOM_2,
+            HIDDEN_ITEM_MT_MOON_B1F_TINY_MUSHROOM_3,
             0xFF,
             0xFF,
             0xFF,
@@ -225,31 +225,31 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
         .mapGroup = MAP_GROUP(THREE_ISLAND_BERRY_FOREST),
         .mapNum = MAP_NUM(THREE_ISLAND_BERRY_FOREST),
         .flags1 = {
-            FLAG_0x443 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x445 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x446 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x447 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x44B - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x44C - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x44D - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x44E - FLAG_HIDDEN_ITEMS_START
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_BLUK_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_WEPEAR_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_ORAN_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_CHERI_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_ASPEAR_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_PERSIM_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_PINAP_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_LUM_BERRY
         },
         .flags2 = {
-            FLAG_0x443 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x445 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x446 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x447 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x44B - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x44C - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x44D - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_BLUK_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_WEPEAR_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_ORAN_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_CHERI_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_ASPEAR_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_PERSIM_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_PINAP_BERRY,
             0xFF
         },
         .flags3 = {
-            FLAG_0x442 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x444 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x448 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x449 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x44A - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_RAZZ_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_NANAB_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_CHESTO_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_PECHA_BERRY,
+            HIDDEN_ITEM_THREE_ISLAND_BERRY_FOREST_RAWST_BERRY,
             0xFF,
             0xFF,
             0xFF
@@ -259,28 +259,28 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
         .mapGroup = MAP_GROUP(ONE_ISLAND_TREASURE_BEACH),
         .mapNum = MAP_NUM(ONE_ISLAND_TREASURE_BEACH),
         .flags1 = {
-            FLAG_0x453 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x454 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x455 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x456 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_ONE_ISLAND_TREASURE_BEACH_ULTRA_BALL,
+            HIDDEN_ITEM_ONE_ISLAND_TREASURE_BEACH_ULTRA_BALL_2,
+            HIDDEN_ITEM_ONE_ISLAND_TREASURE_BEACH_STAR_PIECE,
+            HIDDEN_ITEM_ONE_ISLAND_TREASURE_BEACH_BIG_PEARL,
             0xFF,
             0xFF,
             0xFF,
             0xFF
         },
         .flags2 = {
-            FLAG_0x44F - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x450 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x451 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x452 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x453 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x454 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_ONE_ISLAND_TREASURE_BEACH_STARDUST,
+            HIDDEN_ITEM_ONE_ISLAND_TREASURE_BEACH_STARDUST_2,
+            HIDDEN_ITEM_ONE_ISLAND_TREASURE_BEACH_PEARL,
+            HIDDEN_ITEM_ONE_ISLAND_TREASURE_BEACH_PEARL_2,
+            HIDDEN_ITEM_ONE_ISLAND_TREASURE_BEACH_ULTRA_BALL,
+            HIDDEN_ITEM_ONE_ISLAND_TREASURE_BEACH_ULTRA_BALL_2,
             0xFF,
             0xFF
         },
         .flags3 = {
-            FLAG_0x453 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x454 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_ONE_ISLAND_TREASURE_BEACH_ULTRA_BALL,
+            HIDDEN_ITEM_ONE_ISLAND_TREASURE_BEACH_ULTRA_BALL_2,
             0xFF,
             0xFF,
             0xFF,
@@ -303,8 +303,8 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
             0xFF
         },
         .flags2 = {
-            FLAG_0x48E - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x48F - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_THREE_ISLAND_BOND_BRIDGE_PEARL,
+            HIDDEN_ITEM_THREE_ISLAND_BOND_BRIDGE_STARDUST,
             0xFF,
             0xFF,
             0xFF,
@@ -337,7 +337,7 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
             0xFF
         },
         .flags2 = {
-            FLAG_0x490 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_FOUR_ISLAND_PEARL,
             0xFF,
             0xFF,
             0xFF,
@@ -347,7 +347,7 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
             0xFF
         },
         .flags3 = {
-            FLAG_0x491 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_FOUR_ISLAND_ULTRA_BALL,
             0xFF,
             0xFF,
             0xFF,
@@ -361,7 +361,7 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
         .mapGroup = MAP_GROUP(FIVE_ISLAND_MEMORIAL_PILLAR),
         .mapNum = MAP_NUM(FIVE_ISLAND_MEMORIAL_PILLAR),
         .flags1 = {
-            FLAG_0x492 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_FIVE_ISLAND_MEMORIAL_PILLAR_BIG_PEARL,
             0xFF,
             0xFF,
             0xFF,
@@ -395,8 +395,8 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
         .mapGroup = MAP_GROUP(FIVE_ISLAND_RESORT_GORGEOUS),
         .mapNum = MAP_NUM(FIVE_ISLAND_RESORT_GORGEOUS),
         .flags1 = {
-            FLAG_0x496 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x498 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_FIVE_ISLAND_RESORT_GORGEOUS_NEST_BALL,
+            HIDDEN_ITEM_FIVE_ISLAND_RESORT_GORGEOUS_STAR_PIECE,
             0xFF,
             0xFF,
             0xFF,
@@ -405,8 +405,8 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
             0xFF
         },
         .flags2 = {
-            FLAG_0x497 - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x499 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_FIVE_ISLAND_RESORT_GORGEOUS_STARDUST,
+            HIDDEN_ITEM_FIVE_ISLAND_RESORT_GORGEOUS_STARDUST_2,
             0xFF,
             0xFF,
             0xFF,
@@ -429,8 +429,8 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
         .mapGroup = MAP_GROUP(SIX_ISLAND_OUTCAST_ISLAND),
         .mapNum = MAP_NUM(SIX_ISLAND_OUTCAST_ISLAND),
         .flags1 = {
-            FLAG_0x49A - FLAG_HIDDEN_ITEMS_START,
-            FLAG_0x49B - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_SIX_ISLAND_OUTCAST_ISLAND_STAR_PIECE,
+            HIDDEN_ITEM_SIX_ISLAND_OUTCAST_ISLAND_NET_BALL,
             0xFF,
             0xFF,
             0xFF,
@@ -483,7 +483,7 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
             0xFF
         },
         .flags3 = {
-            FLAG_0x49C - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_SIX_ISLAND_GREEN_PATH_ULTRA_BALL,
             0xFF,
             0xFF,
             0xFF,
@@ -497,7 +497,7 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
         .mapGroup = MAP_GROUP(SEVEN_ISLAND_TRAINER_TOWER),
         .mapNum = MAP_NUM(SEVEN_ISLAND_TRAINER_TOWER),
         .flags1 = {
-            FLAG_0x4A1 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_SEVEN_ISLAND_TRAINER_TOWER_BIG_PEARL,
             0xFF,
             0xFF,
             0xFF,
@@ -507,7 +507,7 @@ const struct UnkStruct_8479D34 gUnknown_8479D34[] = {
             0xFF
         },
         .flags2 = {
-            FLAG_0x4A2 - FLAG_HIDDEN_ITEMS_START,
+            HIDDEN_ITEM_SEVEN_ISLAND_TRAINER_TOWER_PEARL,
             0xFF,
             0xFF,
             0xFF,

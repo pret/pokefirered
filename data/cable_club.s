596cf3a9cb5388c768bbe353b5e9bbef8dd83716
PikalaxALT <pikalaxalt@gmail.com>
2019-10-31 10:25:18 -0400

    Split three files from data_835B488 and dump their data

---
+++ data/cable_club.s
@@ -0,0 +1,15 @@
+	.include "asm/macros.inc"
+	.include "constants/constants.inc"
+
+	.section .rodata
+	.align 2
+
+gUnknown_83C6AB0:: @ 83C6AB0
+	.byte 0, 16, 11, 11, 2, 15
+	.2byte 0x125
+
+gUnknown_83C6AB8:: @ 83C6AB8
+	.4byte gUnknown_841DF8B
+	.4byte gUnknown_841DF92
+	.4byte gUnknown_841DF99
+	.4byte gUnknown_841DFA0

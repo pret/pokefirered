4b33546727007829bb80121790ff377f4e9739b9
Evan <eroelke@gmail.com>
2019-11-26 16:54:55 -0700

    DoPokeballSendOutAnimation

--- src/pokeball.c
+++ src/pokeball.c
@@ -1,1 +1,13 @@
 #include "global.h"
+#include "pokeball.h"
+#include "battle.h"
+
+
+u8 DoPokeballSendOutAnimation(u16 a0, u8 a1)
+{
+	gDoingBattleAnim = 1;
+	gBattleSpritesDataPtr->healthBoxesData[gActiveBattler].ballAnimActive = TRUE;
+	CreateTask(sub_804A9A0, 5);
+
+	return 0;
+}

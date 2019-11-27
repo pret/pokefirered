72d1bbd73fd02b601eedcfbdb15b58adbf569729
PikalaxALT <pikalaxalt@gmail.com>
2019-11-27 09:24:06 -0500

    Cleanup

--- data/pokemon_summary_screen.s
+++ data/pokemon_summary_screen.s
@@ -3,77 +3,75 @@
 	.section .rodata
 	.align 2
 
-gUnknown_84636C0:: @ 84636C0 gbapal
+gUnknown_84636C0:: @ 84636C0
 	.incbin "graphics/interface/pokesummary_unk_84636C0.gbapal"
 
-gUnknown_84636E0:: @ 84636E0 gbapal
+gUnknown_84636E0:: @ 84636E0
 	.incbin "graphics/interface/pokesummary_unk_84636E0.gbapal"
 
-gUnknown_8463700:: @ 8463700 gbapal
+gUnknown_8463700:: @ 8463700
 	.incbin "graphics/interface/pokesummary_unk_8463700.gbapal"
 
-gUnknown_8463720:: @ 8463720 gbapal
+gUnknown_8463720:: @ 8463720
 	.incbin "graphics/interface/pokesummary_unk_8463720.gbapal"
 
-gUnknown_8463740:: @ 8463740 something.lz
+gUnknown_8463740:: @ 8463740
 	.incbin "graphics/interface/pokesummary_unk_8463740.4bpp.lz"
 
-gUnknown_846386C:: @ 846386C something.lz
+gUnknown_846386C:: @ 846386C
 	.incbin "graphics/interface/pokesummary_unk_846386C.4bpp.lz"
 
-gUnknown_846398C:: @ 846398C oamdata
+gUnknown_846398C:: @ 846398C
 	.4byte 0xc0004000, 0x00000000
 
-
-gUnknown_8463994::
+gUnknown_8463994:: @ 8463994
 	obj_image_anim_frame 0x0000, 20
 	obj_image_anim_jump 0
 
-gUnknown_846399C::
+gUnknown_846399C:: @ 846399C
 	obj_image_anim_frame 0x0020, 20
 	obj_image_anim_jump 0
 
-gUnknown_84639A4:: @ 84639A4 dataptr
+gUnknown_84639A4:: @ 84639A4
 	.4byte gUnknown_8463994
 	.4byte gUnknown_846399C
 
-gUnknown_84639AC:: @ 84639AC oamdata
+gUnknown_84639AC:: @ 84639AC
 	.4byte 0x40004000, 0x00000000
 
-
-gUnknown_84639B4::
+gUnknown_84639B4:: @ 84639B4
 	obj_image_anim_frame 0x0000, 20
 	obj_image_anim_jump 0
 
-gUnknown_84639BC::
+gUnknown_84639BC:: @ 84639BC
 	obj_image_anim_frame 0x0004, 20
 	obj_image_anim_jump 0
 
-gUnknown_84639C4::
+gUnknown_84639C4:: @ 84639C4
 	obj_image_anim_frame 0x0008, 20
 	obj_image_anim_jump 0
 
-gUnknown_84639CC::
+gUnknown_84639CC:: @ 84639CC
 	obj_image_anim_frame 0x000c, 20
 	obj_image_anim_jump 0
 
-gUnknown_84639D4::
+gUnknown_84639D4:: @ 84639D4
 	obj_image_anim_frame 0x0010, 20
 	obj_image_anim_jump 0
 
-gUnknown_84639DC::
+gUnknown_84639DC:: @ 84639DC
 	obj_image_anim_frame 0x0014, 20
 	obj_image_anim_jump 0
 
-gUnknown_84639E4::
+gUnknown_84639E4:: @ 84639E4
 	obj_image_anim_frame 0x0018, 20
 	obj_image_anim_jump 0
 
-gUnknown_84639EC::
+gUnknown_84639EC:: @ 84639EC
 	obj_image_anim_frame 0x001c, 20
 	obj_image_anim_jump 0
 
-gUnknown_84639F4:: @ 84639F4 dataptr
+gUnknown_84639F4:: @ 84639F4
 	.4byte gUnknown_84639B4
 	.4byte gUnknown_84639BC
 	.4byte gUnknown_84639C4
@@ -83,58 +81,58 @@ gUnknown_84639F4:: @ 84639F4 dataptr
 	.4byte gUnknown_84639E4
 	.4byte gUnknown_84639EC
 
-gUnknown_8463A14:: @ 8463A14 oamdata
+gUnknown_8463A14:: @ 8463A14
 	.4byte 0x00000000, 0x00000000
 
-gUnknown_8463A1C::
+gUnknown_8463A1C:: @ 8463A1C
 	obj_image_anim_frame 0x0000, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463A24::
+gUnknown_8463A24:: @ 8463A24
 	obj_image_anim_frame 0x0001, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463A2C::
+gUnknown_8463A2C:: @ 8463A2C
 	obj_image_anim_frame 0x0002, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463A34::
+gUnknown_8463A34:: @ 8463A34
 	obj_image_anim_frame 0x0003, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463A3C::
+gUnknown_8463A3C:: @ 8463A3C
 	obj_image_anim_frame 0x0004, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463A44::
+gUnknown_8463A44:: @ 8463A44
 	obj_image_anim_frame 0x0005, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463A4C::
+gUnknown_8463A4C:: @ 8463A4C
 	obj_image_anim_frame 0x0006, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463A54::
+gUnknown_8463A54:: @ 8463A54
 	obj_image_anim_frame 0x0007, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463A5C::
+gUnknown_8463A5C:: @ 8463A5C
 	obj_image_anim_frame 0x0008, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463A64::
+gUnknown_8463A64:: @ 8463A64
 	obj_image_anim_frame 0x0009, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463A6C::
+gUnknown_8463A6C:: @ 8463A6C
 	obj_image_anim_frame 0x000a, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463A74::
+gUnknown_8463A74:: @ 8463A74
 	obj_image_anim_frame 0x000b, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463A7C:: @ 8463A7C dataptr
+gUnknown_8463A7C:: @ 8463A7C
 	.4byte gUnknown_8463A1C
 	.4byte gUnknown_8463A24
 	.4byte gUnknown_8463A2C
@@ -148,123 +146,123 @@ gUnknown_8463A7C:: @ 8463A7C dataptr
 	.4byte gUnknown_8463A6C
 	.4byte gUnknown_8463A74
 
-gUnknown_8463AAC:: @ 8463AAC gbapal
+gUnknown_8463AAC:: @ 8463AAC
 	.incbin "graphics/interface/pokesummary_unk_8463AAC.gbapal"
 
-gUnknown_8463ACC:: @ 8463ACC gbapal
+gUnknown_8463ACC:: @ 8463ACC
 	.incbin "graphics/interface/pokesummary_unk_8463ACC.gbapal"
 
-gUnknown_8463AEC:: @ 8463AEC oamdata
+gUnknown_8463AEC:: @ 8463AEC
 	.4byte 0x00000000, 0x00000000
 
-gUnknown_8463AF4::
+gUnknown_8463AF4:: @ 8463AF4
 	obj_image_anim_frame 0x0000, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463AFC:: @ 8463AFC dataptr
+gUnknown_8463AFC:: @ 8463AFC
 	.4byte gUnknown_8463AF4
 
-gUnknown_8463B00:: @ 8463B00 gbapal
+gUnknown_8463B00:: @ 8463B00
 	.incbin "graphics/interface/pokesummary_unk_8463B00.gbapal"
 
-gUnknown_8463B20:: @ 8463B20 something.lz
+gUnknown_8463B20:: @ 8463B20
 	.incbin "graphics/interface/pokesummary_unk_8463B20.4bpp.lz"
 
-gUnknown_8463B30:: @ 8463B30 oamdata
+gUnknown_8463B30:: @ 8463B30
 	.4byte 0x00000000, 0x00000000
 
-gUnknown_8463B38::
+gUnknown_8463B38:: @ 8463B38
 	obj_image_anim_frame 0x0001, 20
 	obj_image_anim_jump 0
 
-gUnknown_8463B40:: @ 8463B40 dataptr
+gUnknown_8463B40:: @ 8463B40
 	.4byte gUnknown_8463B38
 
-gUnknown_8463B44:: @ 8463B44 gbapal
+gUnknown_8463B44:: @ 8463B44
 	.incbin "graphics/interface/pokesummary_unk_8463B44.gbapal"
 
-gUnknown_8463B64:: @ 8463B64 something.lz
+gUnknown_8463B64:: @ 8463B64
 	.incbin "graphics/interface/pokesummary_unk_8463B64.4bpp.lz"
 
-gUnknown_8463B88:: @ 8463B88 bin.lz
+gUnknown_8463B88:: @ 8463B88
 	.incbin "graphics/interface/pokesummary_unk_8463B88.bin.lz"
 
-gUnknown_8463C80:: @ 8463C80 bin.lz
+gUnknown_8463C80:: @ 8463C80
 	.incbin "graphics/interface/pokesummary_unk_8463C80.bin.lz"
 
-gUnknown_8463DBC::
+gUnknown_8463DBC:: @ 8463DBC
 	.string "HARDY$"
 
-gUnknown_8463DC2::
+gUnknown_8463DC2:: @ 8463DC2
 	.string "LONELY$"
 
-gUnknown_8463DC9::
+gUnknown_8463DC9:: @ 8463DC9
 	.string "BRAVE$"
 
-gUnknown_8463DCF::
+gUnknown_8463DCF:: @ 8463DCF
 	.string "ADAMANT$"
 
-gUnknown_8463DD7::
+gUnknown_8463DD7:: @ 8463DD7
 	.string "NAUGHTY$"
 
-gUnknown_8463DDF::
+gUnknown_8463DDF:: @ 8463DDF
 	.string "BOLD$"
 
-gUnknown_8463DE4::
+gUnknown_8463DE4:: @ 8463DE4
 	.string "DOCILE$"
 
-gUnknown_8463DEB::
+gUnknown_8463DEB:: @ 8463DEB
 	.string "RELAXED$"
 
-gUnknown_8463DF3::
+gUnknown_8463DF3:: @ 8463DF3
 	.string "IMPISH$"
 
-gUnknown_8463DFA::
+gUnknown_8463DFA:: @ 8463DFA
 	.string "LAX$"
 
-gUnknown_8463DFE::
+gUnknown_8463DFE:: @ 8463DFE
 	.string "TIMID$"
 
-gUnknown_8463E04::
+gUnknown_8463E04:: @ 8463E04
 	.string "HASTY$"
 
-gUnknown_8463E0A::
+gUnknown_8463E0A:: @ 8463E0A
 	.string "SERIOUS$"
 
-gUnknown_8463E12::
+gUnknown_8463E12:: @ 8463E12
 	.string "JOLLY$"
 
-gUnknown_8463E18::
+gUnknown_8463E18:: @ 8463E18
 	.string "NAIVE$"
 
-gUnknown_8463E1E::
+gUnknown_8463E1E:: @ 8463E1E
 	.string "MODEST$"
 
-gUnknown_8463E25::
+gUnknown_8463E25:: @ 8463E25
 	.string "MILD$"
 
-gUnknown_8463E2A::
+gUnknown_8463E2A:: @ 8463E2A
 	.string "QUIET$"
 
-gUnknown_8463E30::
+gUnknown_8463E30:: @ 8463E30
 	.string "BASHFUL$"
 
-gUnknown_8463E38::
+gUnknown_8463E38:: @ 8463E38
 	.string "RASH$"
 
-gUnknown_8463E3D::
+gUnknown_8463E3D:: @ 8463E3D
 	.string "CALM$"
 
-gUnknown_8463E42::
+gUnknown_8463E42:: @ 8463E42
 	.string "GENTLE$"
 
-gUnknown_8463E49::
+gUnknown_8463E49:: @ 8463E49
 	.string "SASSY$"
 
-gUnknown_8463E4F::
+gUnknown_8463E4F:: @ 8463E4F
 	.string "CAREFUL$"
 
-gUnknown_8463E57::
+gUnknown_8463E57:: @ 8463E57
 	.string "QUIRKY$"
 
 	.align 2
@@ -295,13 +293,13 @@ gNatureNames:: @ 8463E60 dataptr
 	.4byte gUnknown_8463E4F
 	.4byte gUnknown_8463E57
 
-gUnknown_8463EC4:: @ 8463EC4 dataptr
+gUnknown_8463EC4:: @ 8463EC4
 	.4byte gUnknown_8419B44
 	.4byte gUnknown_8419B7B
 	.4byte gUnknown_8419BAE
 	.4byte gUnknown_8419BDB
 
-gUnknown_8463ED4:: @ 8463ED4 dataptr
+gUnknown_8463ED4:: @ 8463ED4
 	.4byte gUnknown_8419A3D
 	.4byte gUnknown_8419B18
 	.4byte gUnknown_8419A6E
@@ -310,10 +308,10 @@ gUnknown_8463ED4:: @ 8463ED4 dataptr
 	.4byte gUnknown_8419ADE
 	.4byte gUnknown_8419B18
 
-gUnknown_8463EF0:: @ 8463EF0 data8
+gUnknown_8463EF0:: @ 8463EF0
 	.byte 0x00, 0x07, 0x08, 0x00, 0x01, 0x02, 0x00, 0x03, 0x04, 0x00, 0x05, 0x06
 
-gUnknown_8463EFC:: @ 8463EFC bg_template
+gUnknown_8463EFC:: @ 8463EFC
 	.4byte 0x000002e0
 	@ {
 	@ 	.bg = 0,
@@ -355,7 +353,7 @@ gUnknown_8463EFC:: @ 8463EFC bg_template
 	@ 	.baseTile = 0x0000
 	@ }
 
-gUnknown_8463F0C:: @ 8463F0C window_template
+gUnknown_8463F0C:: @ 8463F0C
 	.byte 1, 0, 0, 13, 2, 7
 	.2byte 0x0258
 	.byte 1, 19, 0, 11, 2, 7
@@ -363,7 +361,7 @@ gUnknown_8463F0C:: @ 8463F0C window_template
 	.byte 1, 0, 2, 15, 2, 7
 	.2byte 0x0288
 
-gUnknown_8463F24:: @ 8463F24 window_template
+gUnknown_8463F24:: @ 8463F24
 	.byte 2, 0, 0, 13, 2, 7
 	.2byte 0x0258
 	.byte 2, 19, 0, 11, 2, 7
@@ -371,7 +369,7 @@ gUnknown_8463F24:: @ 8463F24 window_template
 	.byte 2, 0, 2, 15, 2, 7
 	.2byte 0x0288
 
-gUnknown_8463F3C:: @ 8463F3C window_template
+gUnknown_8463F3C:: @ 8463F3C
 	.byte 0, 15, 2, 15, 12, 6
 	.2byte 0x0001
 	.byte 0, 1, 14, 28, 6, 6
@@ -381,7 +379,7 @@ gUnknown_8463F3C:: @ 8463F3C window_template
 	.byte 0, 0, 0, 0, 0, 0
 	.2byte 0x0000
 
-gUnknown_8463F5C:: @ 8463F5C window_template
+gUnknown_8463F5C:: @ 8463F5C
 	.byte 0, 20, 2, 10, 14, 6
 	.2byte 0x0001
 	.byte 0, 6, 12, 14, 4, 6
@@ -391,7 +389,7 @@ gUnknown_8463F5C:: @ 8463F5C window_template
 	.byte 0, 0, 0, 0, 0, 0
 	.2byte 0x0000
 
-gUnknown_8463F7C:: @ 8463F7C window_template
+gUnknown_8463F7C:: @ 8463F7C
 	.byte 0, 20, 2, 10, 18, 8
 	.2byte 0x0001
 	.byte 0, 0, 7, 15, 13, 6
@@ -401,41 +399,41 @@ gUnknown_8463F7C:: @ 8463F7C window_template
 	.byte 0, 6, 4, 9, 2, 6
 	.2byte 0x01d2
 
-gUnknown_8463F9C:: @ 8463F9C window_template
+gUnknown_8463F9C:: @ 8463F9C
 	.byte 255, 0, 0, 0, 0, 0
 	.2byte 0x0000
 
-gUnknown_8463FA4:: @ 8463FA4 data8
+gUnknown_8463FA4:: @ 8463FA4
 	.byte 0x00, 0x0e, 0x0a
 
-gUnknown_8463FA7:: @ 8463FA7 data8
+gUnknown_8463FA7:: @ 8463FA7
 	.byte 0x00, 0x01, 0x02, 0x00, 0x09, 0x08, 0x00, 0x05, 0x04, 0x00, 0x02, 0x03, 0x00, 0x0b, 0x0a, 0x00, 0x00
 
-gUnknown_8463FB8:: @ 8463FB8 data8
+gUnknown_8463FB8:: @ 8463FB8
 	.byte 0x00, 0x02, 0x03, 0x01, 0x04, 0x05
 
-gUnknown_8463FBE:: @ 8463FBE data8
+gUnknown_8463FBE:: @ 8463FBE
 	.byte 0xff, 0x00, 0x01
 
-gUnknown_8463FC1:: @ 8463FC1 data8
+gUnknown_8463FC1:: @ 8463FC1
 	.byte 0xfe, 0xff, 0x00, 0x01, 0x02
 
-gUnknown_8463FC6:: @ 8463FC6 data8
+gUnknown_8463FC6:: @ 8463FC6
 	.byte 0xfd, 0xfe, 0xff, 0x00, 0x01, 0x02, 0x03
 
-gUnknown_8463FCD:: @ 8463FCD data8
+gUnknown_8463FCD:: @ 8463FCD
 	.byte 0xfb, 0xfd, 0xff, 0x00, 0x01, 0x03, 0x05
 
-gUnknown_8463FD4:: @ 8463FD4 data8
+gUnknown_8463FD4:: @ 8463FD4
 	.byte 0x01, 0x01, 0x00, 0xff, 0xff, 0x00, 0xff, 0xff, 0x00, 0x01, 0x01
 
-gUnknown_8463FDF:: @ 8463FDF data8
+gUnknown_8463FDF:: @ 8463FDF
 	.byte 0x02, 0x01, 0x00, 0xff, 0xfe, 0x00, 0xfe, 0xff, 0x00, 0x01, 0x02
 
-gUnknown_8463FEA:: @ 8463FEA data8
+gUnknown_8463FEA:: @ 8463FEA
 	.byte 0x02, 0x01, 0x01, 0x00, 0xff, 0xff, 0xfe, 0x00, 0xfe, 0xff, 0xff, 0x00, 0x01, 0x01, 0x02, 0x00, 0x00, 0x00
 
-gUnknown_8463FFC:: @ 8463FFC dataptr
+gUnknown_8463FFC:: @ 8463FFC
 	.4byte gUnknown_8E9B578
 	.4byte gUnknown_8463AAC
 	.4byte gUnknown_8463ACC

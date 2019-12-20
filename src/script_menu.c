67cf45f7b37ffb4a31528760822ac62dd99931d5
PikalaxALT <pikalaxalt@gmail.com>
2019-12-20 13:53:56 -0500

    Match player name choices in LG

--- src/script_menu.c
+++ src/script_menu.c
@@ -316,11 +316,19 @@ static const struct MenuAction sScriptMultiChoiceMenu_BikeShop[] = {
 };
 
 static const struct MenuAction sScriptMultiChoiceMenu_GameCornerPokemonPrizes[] = {
+#if defined(FIRERED)
     { gText_Abra_180Coins },
     { gText_Clefairy_500Coins },
     { gText_Dratini_2800Coins },
     { gText_Scyther_5500Coins },
     { gText_Porygon_9999Coins },
+#elif defined(LEAFGREEN)
+    { gText_Abra_120Coins },
+    { gText_Clefairy_750Coins },
+    { gText_Pinsir_2500Coins },
+    { gText_Dratini_4600Coins },
+    { gText_Porygon_6500Coins },
+#endif
     { gText_NoThanks_2 }
 };
 

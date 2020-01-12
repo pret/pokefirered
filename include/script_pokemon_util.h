483613e128d8dc5b419ea9ff83c92aa2e4e55042
PikalaxALT <PikalaxALT@users.noreply.github.com>
2020-01-12 14:45:53 -0500

    Fix header guard

--- include/script_pokemon_util.h
+++ include/script_pokemon_util.h
@@ -1,5 +1,5 @@
-#ifndef GUARD_SCRIPT_POKEMON_UTIL_80A0058_H
-#define GUARD_SCRIPT_POKEMON_UTIL_80A0058_H
+#ifndef GUARD_SCRIPT_POKEMON_UTIL_H
+#define GUARD_SCRIPT_POKEMON_UTIL_H
 
 bool8 ScriptGiveMon(u16 species, u8 level, u16 item, u32 unk1, u32 unk2, u8 unk3);
 bool8 ScriptGiveEgg(u16 species);
@@ -8,4 +8,4 @@ void HealPlayerParty(void);
 void ReducePlayerPartyToThree(void);
 void CreateScriptedWildMon(u16 species, u8 level, u16 item);
 
-#endif //GUARD_SCRIPT_POKEMON_UTIL_80A0058_H
+#endif //GUARD_SCRIPT_POKEMON_UTIL_H

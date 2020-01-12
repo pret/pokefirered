0676e732869d0e62322b001cbab57b8500361005
GriffinR <griffin.g.richards@gmail.com>
2020-01-12 16:53:55 -0500

    Remove unused link groups, dead label

--- include/constants/union_room.h
+++ include/constants/union_room.h
@@ -10,19 +10,6 @@
 #define LINK_GROUP_BERRY_PICKING        6
 #define LINK_GROUP_WONDER_CARD          7
 #define LINK_GROUP_WONDER_NEWS          8
-#define LINK_GROUP_UNK_9                9
-#define LINK_GROUP_UNK_10               10
-#define LINK_GROUP_UNK_11               11
-#define LINK_GROUP_RECORD_CORNER        12
-#define LINK_GROUP_BERRY_BLENDER        13
-#define LINK_GROUP_UNK_14               14
-#define LINK_GROUP_COOL_CONTEST         15
-#define LINK_GROUP_BEAUTY_CONTEST       16
-#define LINK_GROUP_CUTE_CONTEST         17
-#define LINK_GROUP_SMART_CONTEST        18
-#define LINK_GROUP_TOUGH_CONTEST        19
-#define LINK_GROUP_BATTLE_TOWER         20
-#define LINK_GROUP_BATTLE_TOWER_OPEN    21
-#define NUM_LINK_GROUP_TYPES            22
+#define NUM_LINK_GROUP_TYPES            9
 
 #endif //GUARD_CONSTANTS_UNION_ROOM_H

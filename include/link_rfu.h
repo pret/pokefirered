11fd159ae811866fcbfdeb7ec558b5809dda9c50
PikalaxALT <pikalaxalt@gmail.com>
2020-01-13 11:17:38 -0500

    Port link_rfu from Emerald

--- include/link_rfu.h
+++ include/link_rfu.h
@@ -9,6 +9,65 @@ struct Padded_U8
     u8 value;
 };
 
+struct UnkLinkRfuStruct_02022B2C
+{
+    u8 unk_00;
+    u8 unk_01;
+    u16 unk_02;
+    u8 unk_04;
+    u16 unk_06;
+    struct UnkLinkRfuStruct_02022B14 *unk_08;
+    u8 *unk_0c;
+    u8 unk_10;
+    u8 unk_11;
+    u16 unk_12;
+    u16 unk_14;
+};
+
+struct UnkRfuStruct_1
+{
+    /* 0x000 */ u8 unk_00;
+    /* 0x001 */ u8 unk_01;
+    /* 0x002 */ vu8 unk_02;
+    /* 0x003 */ vu8 unk_03;
+    /* 0x004 */ u8 unk_04;
+    /* 0x005 */ u8 unk_05;
+    /* 0x006 */ u8 unk_06;
+    /* 0x007 */ u8 unk_07;
+    /* 0x008 */ u8 unk_08;
+    /* 0x009 */ u8 unk_09;
+    /* 0x00a */ u8 unk_0a;
+    /* 0x00b */ u8 unk_0b;
+    /* 0x00c */ u8 unk_0c;
+    /* 0x00d */ u8 unk_0d;
+    /* 0x00e */ u8 unk_0e;
+    /* 0x00f */ u8 unk_0f;
+    /* 0x010 */ u8 unk_10;
+    /* 0x011 */ u8 unk_11;
+    /* 0x012 */ u8 unk_12;
+    // aligned
+    /* 0x014 */ u16 unk_14;
+    /* 0x016 */ u16 unk_16;
+    /* 0x018 */ u16 unk_18;
+    /* 0x01a */ u16 unk_1a;
+    /* 0x01c */ u16 unk_1c;
+    /* 0x01e */ u16 unk_1e;
+    /* 0x020 */ const u16 *unk_20;
+    /* 0x024 */ u8 unk_24;
+    /* 0x026 */ u16 unk_26;
+    /* 0x028 */ u16 unk_28[4];
+    /* 0x030 */ u8 unk_30;
+    // aligned
+    /* 0x032 */ u16 unk_32;
+    /* 0x034 */ u16 unk_34[4];
+    /* 0x03c */ const struct UnkLinkRfuStruct_02022B2C *unk_3c;
+    /* 0x040 */ void (*unk_40)(u8, u8);
+    /* 0x044 */ void (*unk_44)(u16);
+    /* 0x048 */ u8 filler_48[8];
+    /* 0x050 */ u32 unk_50[0x399];
+    /* 0xeb4 */ u8 filler_e64[12];
+};
+
 void AddTextPrinterToWindow1(const u8 *str);
 bool32 MG_PrintTextOnWindow1AndWaitButton(u8 * cmdPtr, const u8 * src);
 void sub_80FA190(void);

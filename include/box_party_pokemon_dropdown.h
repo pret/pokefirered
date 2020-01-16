4d0f46f0cdd43d411e5f543b552969d9731b9074
PikalaxALT <pikalaxalt@gmail.com>
2020-01-16 15:41:01 -0500

    box_party_pokemon_dropdown

---
+++ include/box_party_pokemon_dropdown.h
@@ -0,0 +1,14 @@
+#ifndef GUARD_BOX_PARTY_POKEMON_DROPDOWN_H
+#define GUARD_BOX_PARTY_POKEMON_DROPDOWN_H
+
+void sub_80F7AD8(u8 num);
+void sub_80F7B2C(void);
+void sub_80F7B40(void);
+void sub_80F7B80(u8 a0, u8 a1, const void * a2, u16 a3, u16 a4);
+void sub_80F7C7C(u8 a0, const void * a1);
+void sub_80F7CAC(u8 a0, u16 a1, u16 a2);
+void sub_80F7CE8(u8 a0, u16 a1, u16 a2, u16 a3, u16 a4);
+void sub_80F7D30(u8 a0, u8 a1, s8 a2);
+void sub_80F7E54(u8 a0);
+
+#endif //GUARD_BOX_PARTY_POKEMON_DROPDOWN_H

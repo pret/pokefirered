af6f99b9314f75150d9a5681c17ef5f95a9b1269
PikalaxALT <pikalaxalt@gmail.com>
2020-02-18 14:50:24 -0500

    Port pokemon_jump from emerald, 1

--- include/pokemon_jump.h
+++ include/pokemon_jump.h
@@ -2,9 +2,174 @@
 #define GUARD_POKEMON_JUMP_H
 
 #include "main.h"
+#include "link.h"
+
+struct PokemonJump1_MonInfo
+{
+    u16 species;
+    u32 otId;
+    u32 personality;
+};
+
+struct PokemonJump1_82E4
+{
+    int unk0;
+    int unk4;
+    u8 filler8[0x4];
+    u16 unkC;
+    u16 unkE;
+    u16 unk10;
+    u16 unk12;
+    int unk14;
+    int unk18;
+    u8 unk1C[11];
+};
+
+struct PokemonJump2
+{
+    int unk0;
+    u16 unk4;
+    u8 unk6;
+    u8 filler7[0xa - 0x7];
+    u8 unkA;
+    u8 unkB;
+    u8 unkC;
+    u8 unkD;
+    u8 unkE;
+    u8 unkF;
+    u16 filler10;
+    u16 unk12;
+    u16 unk14;
+    u32 unk18;
+    u16 unk1C[5];
+    u8 txtBuff[2][0x40];
+    u8 strBuff[0x100];
+    u16 tilemapBuffer[(0x81a8 - 0x1a6) / 2]; // 0x1A6
+    struct Sprite *unk81A8[MAX_RFU_PLAYERS];
+    struct Sprite *unk81BC[MAX_RFU_PLAYERS];
+    struct Sprite *unk81D0[8];
+    u8 filler81F0[0xC];
+    u8 unk81FC[MAX_RFU_PLAYERS];
+};
+
+struct PokemonJump1Sub
+{
+    u8 unk0;
+    u8 unk1;
+    u16 unk2;
+    u16 unk4;
+    u32 unk8;
+};
+
+struct PokemonJump1
+{
+    MainCallback returnCallback;
+    u8 unk4;
+    u8 unk5;
+    u8 unk6;
+    u8 unk7;
+    u16 unk8;
+    u16 unkA;
+    u16 unkC;
+    u16 unkE;
+    int unk10;
+    u32 unk14;
+    u32 unk18;
+    int unk1C;
+    u32 unk20;
+    u32 unk24;
+    u32 unk28;
+    int unk2C;
+    u32 unk30;
+    u16 unk34;
+    u16 unk36;
+    u8 filler38[0x2];
+    u16 unk3A;
+    u16 unk3C;
+    u16 unk3E;
+    u16 unk40;
+    u16 unk42;
+    u8 unk44;
+    u8 unk45;
+    u8 unk46;
+    u8 isLeader;
+    u8 unk48;
+    u8 unk49;
+    u16 unk4A;
+    u8 unk4C;
+    u8 unk4D;
+    u16 unk4E;
+    u8 unk50;
+    u8 unk51;
+    u8 filler52[0x2];
+    int unk54;
+    int unk58;
+    int unk5C;
+    int unk60;
+    int unk64;
+    int unk68;
+    int unk6C;
+    struct PokemonJump1Sub unk70;
+    u8 unk7C[MAX_RFU_PLAYERS];
+    u8 unk81[MAX_RFU_PLAYERS];
+    u8 unk86[MAX_RFU_PLAYERS];
+    u8 unk8B[MAX_RFU_PLAYERS];
+    u16 unk90[MAX_RFU_PLAYERS];
+    u16 unk9A[MAX_RFU_PLAYERS];
+    struct PokemonJump2 unkA4;
+    struct PokemonJump1_MonInfo unk82A8[MAX_RFU_PLAYERS];
+    struct PokemonJump1_82E4 unk82E4[MAX_RFU_PLAYERS];
+    struct PokemonJump1_82E4 *unk83AC;
+};
+
+struct PokemonJumpMons
+{
+    u16 species;
+    u16 unk2;
+};
 
 void ResetPokeJumpResults(void);
 bool32 IsSpeciesAllowedInPokemonJump(u16 species);
 void StartPokemonJump(u16 species, MainCallback callback);
+u16 sub_81499A4(void);
+u16 sub_81499B4(void);
+struct PokemonJump1_MonInfo *sub_81499C0(u8 multiplayerId);
+u8 *sub_81499E0(u8 multiplayerId);
+
+void sub_814A95C(int id);
+void sub_8149CEC(struct PokemonJump2 *);
+void sub_802D0BC(struct PokemonJump2 *);
+void sub_8149D24(void);
+void sub_8149D40(int);
+bool32 sub_8149D68(void);
+void sub_814A3E4(void);
+bool32 sub_814A408(void);
+void sub_814A468(u16 itemId, u16 quantity);
+void sub_814A53C(u16 itemId);
+void sub_814A5B4(u16 itemId);
+bool32 sub_814A62C(void);
+void sub_814A6CC(void);
+void sub_814A940(u32 id, s16 y);
+void sub_814AA48(u8 multiplayerId);
+bool32 sub_814A6FC(void);
+void sub_814AA24(int);
+s8 sub_814A744(void);
+int sub_814A98C(u8 flags);
+void sub_814AA34(u16);
+void sub_814AA60(u8 multiplayerId);
+int sub_814AA78(int multiplayerId);
+void sub_814AA8C(void);
+void sub_814AAA0(void);
+void sub_814AAB4(int);
+int sub_814AAC8(void);
+void sub_8149A6C(struct PokemonJump1_MonInfo *arg0);
+bool32 sub_8149A90(int multiplayerId, struct PokemonJump1_MonInfo *arg0);
+void sub_8149AF8(struct PokemonJump1_82E4 *arg0, struct PokemonJump1Sub *arg1);
+bool32 sub_8149B7C(struct PokemonJump1_82E4 *arg0, struct PokemonJump1Sub *arg1);
+void sub_8149BF4(struct PokemonJump1_82E4 *arg0, u8 arg1, u16 arg2);
+bool32 sub_8149C24(struct PokemonJump1_82E4 *arg0, int multiplayerId, u8 *arg2, u16 *arg3);
+bool32 sub_8149C90(struct PokemonJump1_82E4 *arg0, int multiplayerId);
+bool32 sub_814B494(u32 jumpScore, u16 jumpsInRow, u16 excellentsInRow);
+void sub_814B4E8(void);
 
 #endif // GUARD_POKEMON_JUMP_H
---
+++ src/pokemon_jump.c
@@ -0,0 +1,254 @@
+#include "global.h"
+#include "gflib.h"
+#include "battle_anim.h"
+#include "decompress.h"
+#include "link.h"
+#include "link_rfu.h"
+#include "task.h"
+#include "constants/songs.h"
+
+static void sub_8147654(u8 taskId);
+static void sub_81477C0(u8 taskId);
+static void sub_814784C(u8 taskId);
+static void sub_81479D4(u8 taskId);
+static void sub_8147A34(u8 taskId);
+
+static const u16 gUnknown_846AFE8[] = INCBIN_U16("graphics/misc/unk_846B008.gbapal");
+static const u32 gUnknown_846B008[] = INCBIN_U32("graphics/misc/unk_846B008.4bpp.lz");
+
+static const struct CompressedSpriteSheet gUnknown_846B42C[] = {
+    {gUnknown_846B008, 0x0C00, 0x2000},
+    {}
+};
+
+static const struct SpritePalette gUnknown_846B43C[] = {
+    {gUnknown_846AFE8, 0x2000},
+    {}
+};
+
+static const union AnimCmd gUnknown_846B44C[] = {
+    ANIMCMD_FRAME(0x00, 0),
+    ANIMCMD_END
+};
+
+static const union AnimCmd gUnknown_846B454[] = {
+    ANIMCMD_FRAME(0x10, 0),
+    ANIMCMD_END
+};
+
+static const union AnimCmd gUnknown_846B45C[] = {
+    ANIMCMD_FRAME(0x20, 0),
+    ANIMCMD_END
+};
+
+static const union AnimCmd gUnknown_846B464[] = {
+    ANIMCMD_FRAME(0x40, 0),
+    ANIMCMD_END
+};
+
+static const union AnimCmd gUnknown_846B46C[] = {
+    ANIMCMD_FRAME(0x30, 0),
+    ANIMCMD_END
+};
+
+static const union AnimCmd gUnknown_846B474[] = {
+    ANIMCMD_FRAME(0x50, 0),
+    ANIMCMD_END
+};
+
+static const union AnimCmd *const gUnknown_846B47C[] = {
+    gUnknown_846B44C,
+    gUnknown_846B454,
+    gUnknown_846B45C,
+    gUnknown_846B464,
+    gUnknown_846B46C,
+    gUnknown_846B474
+};
+
+static const struct SpriteTemplate gUnknown_846B494[] = {
+    {
+        .tileTag = 0x2000,
+        .paletteTag = 0x2000,
+        .oam = &gOamData_AffineOff_ObjNormal_32x32,
+        .anims = gUnknown_846B47C,
+        .affineAnims = gDummySpriteAffineAnimTable,
+        .callback = SpriteCallbackDummy
+    },
+};
+
+static const TaskFunc gUnknown_846B4AC[][4] = {
+    { sub_81477C0, sub_814784C, sub_81479D4, sub_8147A34 }
+};
+
+// Unused, but looks like it was intended to be a hook
+UNUSED u8 sub_81475C0(u8 data1, u8 priority)
+{
+    u8 taskId;
+    struct Task * task;
+
+    taskId = CreateTask(sub_8147654, priority);
+    task = &gTasks[taskId];
+    task->data[0] = 1;
+    task->data[1] = data1;
+    gUnknown_846B4AC[data1][0](taskId);
+    return taskId;
+}
+
+// Unused, but looks like it was intended to be a hook
+UNUSED bool32 sub_814760C(void)
+{
+    u8 taskId;
+
+    taskId = FindTaskIdByFunc(sub_8147654);
+    if (taskId == 0xFF)
+        return FALSE;
+    gTasks[taskId].data[0] = 2;
+    return TRUE;
+}
+
+// Unused, but looks like it was intended to be a hook
+UNUSED bool32 sub_8147640(void)
+{
+    return FuncIsActiveTask(sub_8147654);
+}
+
+static void sub_8147654(u8 taskId)
+{
+    s16 *data = gTasks[taskId].data;
+
+    switch (data[0])
+    {
+    case 2:
+        gUnknown_846B4AC[data[1]][2](taskId);
+        data[0] = 3;
+        break;
+    case 3:
+        gUnknown_846B4AC[data[1]][3](taskId);
+        break;
+    case 4:
+        gUnknown_846B4AC[data[1]][1](taskId);
+        DestroyTask(taskId);
+        break;
+    }
+}
+
+static void sub_81476E0(u8 taskId, s16 *data)
+{
+    u8 i;
+    struct Sprite *sprite;
+
+    LoadCompressedSpriteSheet(&gUnknown_846B42C[data[3]]);
+    LoadSpritePalette(&gUnknown_846B43C[data[4]]);
+    for (i = 0; i < data[8]; i++)
+        data[13 + i] = CreateSprite(&gUnknown_846B494[data[2]], data[9], data[10], data[7]);
+    for (i = 0; i < data[8]; i++)
+    {
+        sprite = &gSprites[data[13 + i]];
+        sprite->oam.priority = data[6];
+        sprite->invisible = TRUE;
+        sprite->data[1] = data[5];
+        sprite->data[3] = taskId;
+        sprite->data[4] = i;
+        sprite->data[5] = data[13];
+    }
+}
+
+static void sub_81477C0(u8 taskId)
+{
+    s16 *data = gTasks[taskId].data;
+    data[2] = 0;
+    data[3] = 0;
+    data[4] = 0;
+    data[5] = 60;
+    data[6] = 0;
+    data[7] = 0;
+    data[8] = 3;
+    data[9] = 120;
+    data[10] = 88;
+    sub_81476E0(taskId, data);
+
+    StartSpriteAnim(&gSprites[data[14]], 4);
+    gSprites[data[14]].pos2.x = -32;
+
+    StartSpriteAnim(&gSprites[data[15]], 5);
+    gSprites[data[15]].pos2.x = 32;
+}
+
+static void sub_814784C(u8 taskId)
+{
+    u8 i = 0;
+    s16 *data = gTasks[taskId].data;
+
+    for (i = 0; i < data[8]; i++)
+        DestroySprite(&gSprites[data[13 + i]]);
+    FreeSpriteTilesByTag(gUnknown_846B42C[data[3]].tag);
+    FreeSpritePaletteByTag(gUnknown_846B43C[data[4]].tag);
+}
+
+static void sub_81478BC(struct Sprite *sprite)
+{
+    s16 *data = gTasks[sprite->data[3]].data;
+
+    if (data[11] % data[5] != 0)
+        return;
+    if (data[11] == data[10])
+        return;
+
+    data[10] = data[11];
+    switch (sprite->data[2])
+    {
+    case 0:
+        sprite->invisible = FALSE;
+    case 1:
+    case 2:
+        PlaySE(SE_KON);
+        StartSpriteAnim(sprite, sprite->data[2]);
+        break;
+    case 3:
+        PlaySE(SE_PIN);
+        StartSpriteAnim(sprite, sprite->data[2]);
+        gSprites[data[14]].invisible = FALSE;
+        gSprites[data[15]].invisible = FALSE;
+        break;
+    case 4:
+        sprite->invisible = TRUE;
+        gSprites[data[14]].invisible = TRUE;
+        gSprites[data[15]].invisible = TRUE;
+        data[0] = 4;
+        return;
+    }
+    sprite->data[2]++;
+}
+
+static void sub_81479D4(u8 taskId)
+{
+    s16 *data = gTasks[taskId].data;
+    PlaySE(SE_KON);
+    gSprites[data[13]].callback = sub_81478BC;
+    gSprites[data[13]].invisible = FALSE;
+    gTasks[taskId].data[0] = 3;
+}
+
+static void sub_8147A34(u8 taskId)
+{
+    u16 packet[6];
+    s16 *data = gTasks[taskId].data;
+
+    if (gReceivedRemoteLinkPlayers != 0)
+    {
+        if (gRecvCmds[0][1] == 0x7FFF)
+            data[11] = gRecvCmds[0][2];
+        if (GetMultiplayerId() == 0)
+        {
+            data[12]++;
+            memset(packet, 0, sizeof(packet));
+            packet[0] = 0x7FFF;
+            packet[1] = data[12];
+            RfuPrepareSend0x2f00(packet);
+        }
+    }
+    else
+    {
+        data[11]++;
+    }
+}

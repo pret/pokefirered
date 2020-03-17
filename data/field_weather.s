8a64f8b431b0d68453f8d4302474926adf407bb1
PikalaxALT <PikalaxALT@gmail.com>
2020-03-17 08:29:24 -0400

    Remove data/field_weather.s

--- data/field_weather.s
+++
@@ -1,8 +0,0 @@
-#include "constants/region_map_sections.h"
-#include "constants/flags.h"
-#include "constants/moves.h"
-#define RGB(r, g, b) ((r) | ((g) << 5) | ((b) << 10))
-	.include "asm/macros.inc"
-	.include "constants/constants.inc"
-	.section .rodata
-	.align 2

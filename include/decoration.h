19812c9d18a64e57ebc2ef5425176b89249b2fef
PikalaxALT <PikalaxALT@gmail.com>
2020-03-29 11:42:08 -0400

    Decoration data to C

---
+++ include/decoration.h
@@ -0,0 +1,55 @@
+#ifndef GUARD_DECORATION_H
+#define GUARD_DECORATION_H
+
+enum DecorationPermission
+{
+    /*
+     * The nomenclature here describes collision and placement permissions, in that order.
+     */
+    DECORPERM_SOLID_FLOOR,
+    DECORPERM_PASS_FLOOR,
+    DECORPERM_BEHIND_FLOOR,
+    DECORPERM_NA_WALL,
+    DECORPERM_SPRITE,
+};
+
+enum DecorationShape
+{
+    DECORSHAPE_1x1,
+    DECORSHAPE_2x1,
+    DECORSHAPE_3x1, // unused
+    DECORSHAPE_4x2,
+    DECORSHAPE_2x2,
+    DECORSHAPE_1x2,
+    DECORSHAPE_1x3, // unused
+    DECORSHAPE_2x4,
+    DECORSHAPE_3x3,
+    DECORSHAPE_3x2,
+};
+
+enum DecorationCategory
+{
+    DECORCAT_DESK,
+    DECORCAT_CHAIR,
+    DECORCAT_PLANT,
+    DECORCAT_ORNAMENT,
+    DECORCAT_MAT,
+    DECORCAT_POSTER,
+    DECORCAT_DOLL,
+    DECORCAT_CUSHION,
+    DECORCAT_COUNT,
+};
+
+struct Decoration
+{
+    u8 id;
+    u8 name[16];
+    u8 permission;
+    u8 shape;
+    u8 category;
+    u16 price;
+    const u8 *description;
+    const u16 *tiles;
+};
+
+#endif //GUARD_DECORATION_H

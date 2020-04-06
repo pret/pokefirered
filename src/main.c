bbeef52a014c1242dd790b14c52d933b75666f7e
PikalaxALT <pikalaxalt@gmail.com>
2020-04-06 13:20:36 -0400

    Timestamp modern build

--- src/main.c
+++ src/main.c
@@ -34,11 +34,15 @@ const u8 gGameVersion = GAME_VERSION;
 
 const u8 gGameLanguage = GAME_LANGUAGE;
 
+#if MODERN
+const char BuildDateTime[] = __DATE__ " " __TIME__;
+#else
 #if REVISION == 0
 const char BuildDateTime[] = "2004 04 26 11:20";
 #else
 const char BuildDateTime[] = "2004 07 20 09:30";
-#endif
+#endif //REVISION
+#endif //MODERN
 
 const IntrFunc gIntrTableTemplate[] =
 {

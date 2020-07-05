8a323538cf5f147205be8b4033274f7f5560fbb0
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2020-07-04 21:23:57 -0400

    Forgot to add some excludes to grep shell scripts.

--- cgrep.sh
+++ cgrep.sh
@@ -7,4 +7,4 @@ else
 	grep_flags="$2"
 fi
 
-grep "$1" "$grep_flags" --include="*.c" --include="*.h" --include="*.s" --include="*.inc" --include="*.txt" --exclude-dir=".git" --exclude-dir=".travis" --exclude-dir="build" --exclude-dir="common_syms" --exclude-dir="data" --exclude-dir="graphics" --exclude-dir="sound" --exclude-dir="tools"
+grep "$1" "$grep_flags" --include="*.c" --include="*.h" --include="*.s" --include="*.inc" --include="*.txt" --exclude-dir=".git" --exclude-dir=".travis" --exclude-dir="build" --exclude-dir="common_syms" --exclude-dir="data" --exclude-dir="graphics" --exclude-dir="sound" --exclude-dir="tools" --exclude-dir="berry_fix/build" --exclude-dir="berry_fix/payload/build"

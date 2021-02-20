cfe5c9358b5370210b4b091a07c5e6efef57cef7
N <71219152+PokeCodec@users.noreply.github.com>
2021-02-20 18:59:18 -0500

    Fix button macro

--- include/global.h
+++ include/global.h
@@ -80,7 +80,7 @@
 // We suspect it was used globally.
 // GameFreak never ceases to amaze.
 // TODO: Propagate use of this macro
-#define TEST_BUTTON(field, button) ({(field) & (button);})
+#define TEST_BUTTON(field, button) ((field) & (button))
 #define JOY_NEW(button)  TEST_BUTTON(gMain.newKeys,  button)
 #define JOY_HELD(button) TEST_BUTTON(gMain.heldKeys, button)
 #define JOY_REPT(button) TEST_BUTTON(gMain.newAndRepeatedKeys, button)

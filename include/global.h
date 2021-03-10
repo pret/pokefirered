0589bd582764cb84bc759943b727973dd9166bbd
Deokishisu <6993375+Deokishisu@users.noreply.github.com>
2021-03-10 16:22:08 -0500

    Fix Typos / Use Box Constants
    The Celadon City scripts that give Eevee misspelled it as Eeevee. Fixed.
    
    The comment explaining `boxRSEggsUnlocked` in the `ExternalEventFlags` struct had an incorrect deposit requirement listed and was fixed.
    
    Also, replaced the literals with the `TOTAL_BOXES_COUNT` and `IN_BOX_COUNT` constants in `pokemon.c`'s `IsPokemonStorageFull` function. Thanks hjk321 for pointing that out.

--- include/global.h
+++ include/global.h
@@ -736,7 +736,7 @@ struct ExternalEventData
 struct ExternalEventFlags
 {
     u8 usedBoxRS:1; // Set by Pokémon Box: Ruby & Sapphire; denotes whether this save has connected to it and triggered the free False Swipe Swablu Egg giveaway.
-    u8 boxRSEggsUnlocked:2; // Set by Pokémon Box: Ruby & Sapphire; denotes the number of Eggs unlocked from deposits; 1 for ExtremeSpeed Zigzagoon (at 100 deposited), 2 for Pay Day Skitty (at 500 deposited), 3 for Surf Pichu (at 1500 deposited)
+    u8 boxRSEggsUnlocked:2; // Set by Pokémon Box: Ruby & Sapphire; denotes the number of Eggs unlocked from deposits; 1 for ExtremeSpeed Zigzagoon (at 100 deposited), 2 for Pay Day Skitty (at 500 deposited), 3 for Surf Pichu (at 1499 deposited)
     u8 padding:5;
     u8 unknownFlag1;
     u8 receivedGCNJirachi; // Both the US Colosseum Bonus Disc and PAL/AUS Pokémon Channel use this field. One cannot receive a WISHMKR Jirachi and CHANNEL Jirachi with the same savefile.

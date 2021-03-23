646c44322180b133097b593503f576719a9fffda
Kurausukun <lord.uber1@gmail.com>
2021-03-22 23:56:38 -0400

    [LEAK-INFORMED] fix Task_UseItem_OutwardSpiralDots fakematching

--- src/pokemon_special_anim_scene.c
+++ src/pokemon_special_anim_scene.c
@@ -1259,23 +1259,14 @@ static void StopMakingOutwardSpiralDots(void)
 static void Task_UseItem_OutwardSpiralDots(u8 taskId)
 {
     s16 *data = gTasks[taskId].data;
-    struct Sprite * sprite;
-    int x;
-    #ifndef NONMATCHING
-        register int y asm("r10"); // FIXME
-    #else
-        int y;
-    #endif
-    int x2;
-    int y2;
-    int ampl;
-    u8 spriteId;
+
     switch (tState)
     {
     case 0:
         if (tTimer == 0)
         {
-            sprite = PSA_GetSceneWork()->itemIconSprite;
+            u32 spriteId, x, y, x2, y2, ampl;
+            struct Sprite * sprite = PSA_GetSceneWork()->itemIconSprite;
             x = sprite->pos1.x + sprite->pos2.x;
             y = sprite->pos1.y + sprite->pos2.y;
             ampl = (PSAScene_RandomFromTask(taskId) % 21) + 70;

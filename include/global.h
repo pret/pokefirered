46867c546d5df8c8b86b5ad46b1c149cc1ad0688
Jademalo <386846+Jademalo@users.noreply.github.com>
2021-10-22 04:42:48 +0100

    Improve IDE Support

--- include/global.h
+++ include/global.h
@@ -16,20 +16,18 @@
 #define asm_comment(x) asm volatile("@ -- " x " -- ")
 #define asm_unified(x) asm(".syntax unified\n" x "\n.syntax divided")
 
-#if defined (__APPLE__) || defined (__CYGWIN__) || defined(__CLION_IDE__)
-// Get the IDE to stfu
-
-// We define it this way to fool preproc.
-#define INCBIN(...) {0}
-#define INCBIN_U8  INCBIN
+// IDE support
+#if defined (__APPLE__) || defined (__CYGWIN__) || defined (__INTELLISENSE__)
+#define _(x) x
+#define __(x) x
+#define INCBIN(x) {0}
+#define INCBIN_U8 INCBIN
 #define INCBIN_U16 INCBIN
 #define INCBIN_U32 INCBIN
-#define INCBIN_S8  INCBIN
+#define INCBIN_S8 INCBIN
 #define INCBIN_S16 INCBIN
 #define INCBIN_S32 INCBIN
-#define _(x) (x)
-#define __(x) (x)
-#endif // __APPLE__
+#endif // IDE support
 
 #define NELEMS(array) (sizeof(array) / sizeof((array)[0]))
 

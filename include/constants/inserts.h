66f14b9651f82b29bd81f11244152e38a514f797
bugdorderoms <93982673+bugdorderoms@users.noreply.github.com>
2021-11-17 19:11:34 -0300

    Update inserts.h

--- include/constants/inserts.h
+++ include/constants/inserts.h
@@ -1,1 +1,2 @@
 #define EXP_BLOCK TRUE //if true go to src/ battle_script_commands.c and edit the levels for each badge in sExpBlockLevels
+#define PAGE_SWAP TRUE //if true auttomaticaly swap the naming screen page on write the first caracter

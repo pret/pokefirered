aac18d2f10ce846ddfa73fb1d481c0d9af50bc89
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2021-11-18 20:18:09 -0300

    Update inserts.h

--- include/constants/inserts.h
+++ include/constants/inserts.h
@@ -1,2 +1,3 @@
-#define EXP_BLOCK TRUE //if true go to src/ battle_script_commands.c and edit the levels for each badge in sExpBlockLevels
-#define PAGE_SWAP TRUE //if true auttomaticaly swap the naming screen page on write the first caracter
+#define EXP_BLOCK TRUE //if true go to src/ battle_script_commands.c and edit the levels for each badge in sExpBlockLevels.
+#define PAGE_SWAP TRUE //if true auttomaticaly swap the naming screen page on write the first caracter.
+#define BW_REPEL_SYSTEM TRUE //if true when the Repel's effect wears off, the game will automatically ask the player whether they want to use another one immediately.

c5c19cb59a05155de18b0012e6e4b440a2ec079f
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2021-11-21 11:11:15 -0300

    Update battle_script_commands.c

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -1041,7 +1041,7 @@ static bool8 AccuracyCalcHelper(u16 move)
     }
     gHitMarker &= ~HITMARKER_IGNORE_UNDERWATER;
     if ((WEATHER_HAS_EFFECT && (gBattleWeather & WEATHER_RAIN_ANY) && gBattleMoves[move].effect == EFFECT_THUNDER)
-     || (gBattleMoves[move].effect == EFFECT_ALWAYS_HIT || gBattleMoves[move].effect == EFFECT_VITAL_THROW))
+     || (gBattleMoves[move].accuracy == 0))
     {
         JumpIfMoveFailed(7, move);
         return TRUE;

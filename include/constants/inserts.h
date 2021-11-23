a3a56eb5fd2319e1c33a7e7eda3b05708aeb96ad
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2021-11-23 20:02:12 -0300

    Update inserts.h

--- include/constants/inserts.h
+++ include/constants/inserts.h
@@ -1,6 +1,7 @@
 #define EXP_BLOCK TRUE //if true go to src/ battle_script_commands.c and edit the levels for each badge in sExpBlockLevels.
 #define PAGE_SWAP TRUE //if true auttomaticaly swap the naming screen page on write the first caracter.
 #define NO_HEAL_EGGS TRUE //if true the pokemon center animation don't coun't eggs
+#define EGG_HATCH_LEVEL_1 TRUE //if true the eggs hatch level becames 1
 
 // need
 #define BW_REPEL_SYSTEM TRUE //if true when the Repel's effect wears off, the game will automatically ask the player whether they want to use another one immediately.

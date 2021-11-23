eb022e209868fbdc11e6a5559bce346629d550c3
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2021-11-23 19:44:03 -0300

    Update pokemon_storage_system.h

--- include/pokemon_storage_system.h
+++ include/pokemon_storage_system.h
@@ -37,5 +37,6 @@ void Cb2_ReturnToPSS(void);
 void ResetPokemonStorageSystem(void);
 u8 StorageGetCurrentBox(void);
 void DrawTextWindowAndBufferTiles(const u8 *string, void *dst, u8 zero1, u8 zero2, u8 *buffer, s32 bytesToBuffer);
+u8 CountPartyNonEggMons(void);
 
 #endif // GUARD_POKEMON_STORAGE_SYSTEM_H

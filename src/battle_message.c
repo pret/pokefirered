0a2f53239e0bfb5240707d0b0fe0460f981c9cbd
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2021-11-28 15:28:43 -0300

    Update battle_message.c

--- src/battle_message.c
+++ src/battle_message.c
@@ -1703,13 +1703,8 @@ void BufferStringBattle(u16 stringId)
     default: // load a string from the table
         if (stringId >= BATTLESTRINGS_COUNT + BATTLESTRINGS_ID_ADDER)
         {
-            if (stringId == STRINGID_SETWORDSTRING) {
+            if (stringId == STRINGID_SETWORDSTRING)
                 stringPtr = gSetWordLoc;
-                if (stringPtr == 0 || stringPtr >= 0xA000000 || stringPtr < 0x8000000) {
-                    stringId = STRINGID_TRAINER1LOSETEXT;
-                    stringPtr = gBattleStringsTable[stringId - BATTLESTRINGS_ID_ADDER];
-                }
-            }
             else
             {
             gDisplayedStringBattle[0] = EOS;

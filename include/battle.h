2d74b719dd4d3e2308190fc37f2ca7f206d731f9
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2021-11-28 15:27:00 -0300

    Update battle.h

--- include/battle.h
+++ include/battle.h
@@ -719,5 +719,6 @@ extern u8 gChosenActionByBattler[MAX_BATTLERS_COUNT];
 extern u8 gBattleTerrain;
 extern struct MultiBattlePokemonTx gMultiPartnerParty[3];
 extern u16 gRandomTurnNumber;
+extern const u8 *gSetWordLoc;
 
 #endif // GUARD_BATTLE_H

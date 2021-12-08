d26a733c3593a9c5d6ed3f7630b655b98ecccb2d
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2021-12-08 13:16:19 -0300

    Update battle_scripts.h

--- include/battle_scripts.h
+++ include/battle_scripts.h
@@ -237,6 +237,8 @@ extern const u8 BattleScript_BadDreamsTurnDmg[];
 extern const u8 BattleScript_DoTurnDmgEnd[];
 extern const u8 BattleScript_Download[];
 extern const u8 BattleScript_DownloadRaiseSpAttack[];
+extern const u8 BattleScript_DrySkinRainActivates[];
+extern const u8 BattleScript_DrySkinSunActivates[];
 
 extern const u8 *const gBattleScriptsForMoveEffects[];
 extern const u8 *const gBattlescriptsForBallThrow[];

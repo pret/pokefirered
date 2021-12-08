2af5f44c34d7d2ba33f741638fc8f0e23b71b46d
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2021-12-08 13:40:32 -0300

    Update pokemon.c

--- src/pokemon.c
+++ src/pokemon.c
@@ -2454,6 +2454,8 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
         attack = (150 * attack) / 100;
     if (defender->ability == ABILITY_MARVEL_SCALE && defender->status1)
         defense = (150 * defense) / 100;
+    if (defender->ability == ABILITY_DRY_SKIN && type == TYPE_FIRE)
+        gBattleMovePower += gBattleMovePower / 4;
     if (type == TYPE_ELECTRIC && AbilityBattleEffects(ABILITYEFFECT_FIELD_SPORT, 0, 0, 0xFD, 0))
         gBattleMovePower /= 2;
     if (type == TYPE_FIRE && AbilityBattleEffects(ABILITYEFFECT_FIELD_SPORT, 0, 0, 0xFE, 0))

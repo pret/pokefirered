dcc00fd58349ed5a9d7bded4067d523c1f384883
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2021-12-23 20:36:34 -0300

    Update battle_script_commands.c

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -1180,7 +1180,7 @@ static void atk01_accuracycheck(void)
         }
         else
         {
-            holdEffect = ItemId_GetHoldEffectCheckKlutz(gBattleMons[gBattlerTarget].item, gBattlerTarget);
+            holdEffect = ItemId_GetHoldEffect(gBattleMons[gBattlerTarget].item, gBattlerTarget, TRUE);
             param = ItemId_GetHoldEffectParam(gBattleMons[gBattlerTarget].item);
         }
         gPotentialItemEffectBattler = gBattlerTarget;

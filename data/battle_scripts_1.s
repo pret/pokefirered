a7e8fc06f43012e2a6ca97c4860b3ceb4f8ea472
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2021-12-25 10:26:31 -0300

    Update battle_scripts_1.s

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -3939,6 +3939,7 @@ BattleScript_MoveEffectConfusion::
 BattleScript_MoveEffectRecoil::
 	jumpifmove MOVE_STRUGGLE, BattleScript_DoRecoil
 	jumpifability BS_ATTACKER, ABILITY_ROCK_HEAD, BattleScript_RecoilEnd
+	jumpifability BS_ATTACKER, ABILITY_MAGIC_GUARD, BattleScript_RecoilEnd
 BattleScript_DoRecoil::
 	orword gHitMarker, HITMARKER_IGNORE_SUBSTITUTE | HITMARKER_PASSIVE_DAMAGE
 	healthbarupdate BS_ATTACKER

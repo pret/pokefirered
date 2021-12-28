1fb6f444fa33a974a1444cd58506b34165ce97b8
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2021-12-28 14:21:17 -0300

    Update battle_script_commands.h

--- include/battle_script_commands.h
+++ include/battle_script_commands.h
@@ -20,6 +20,7 @@ void BattleDestroyYesNoCursorAt(void);
 void HandleBattleWindow(u8 xStart, u8 yStart, u8 xEnd, u8 yEnd, u8 flags);
 u8 GetBattlerTurnOrderNum(u8 battlerId);
 void BufferMoveToLearnIntoBattleTextBuff2(void);
+u8 TryChangeMoveType(u8 battler, u8 type, u16 move);
 
 extern void (* const gBattleScriptingCommandsTable[])(void);
 

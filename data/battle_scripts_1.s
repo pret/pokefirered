39333ecc19b15d8b64e2e4199631d66008608517
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-01-11 10:54:56 -0300

    Update battle_scripts_1.s

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -3040,10 +3040,8 @@ BattleScript_SmokeBallEscape::
 	end2
 
 BattleScript_RanAwayUsingMonAbility::
-        loadabilitypopup LOAD_ABILITY_NORMAL, BS_ATTACKER, LOAD_ABILITY_FROM_BUFFER
 	printstring STRINGID_PKMNFLEDUSING
 	waitmessage 0x40
-	loadabilitypopup REMOVE_POP_UP, BS_ATTACKER, LOAD_ABILITY_FROM_BUFFER
 	end2
 
 BattleScript_GotAwaySafely::

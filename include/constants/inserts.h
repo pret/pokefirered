58d862df11ebb2af4c18b3a896e38494d4444ff5
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-01-16 13:50:30 -0300

    Update inserts.h

--- include/constants/inserts.h
+++ include/constants/inserts.h
@@ -6,10 +6,11 @@
 #define EGG_HATCH_LEVEL_1 TRUE //if true the eggs hatch level becames 1.
 #define POISON_SURVIVAL TRUE //if true the pokemons don't die from poison in the overworld.
 #define SLEEP_UPDATE TRUE //if true the sleep counter last for 1-3 turns instead of 2-5.
+#define PARALYSIS_UPDATE TRUE //if true the speed drop of paralysis will be 50% instead of 75%.
 #define EFFECTIVENESS_ON_MENU TRUE //if true the move type name trade its color based on the move effectiveness in battle.
 #define NATURE_COLOURS TRUE //if true colours the stat in the summary screen depending on the nature.
 #define IVS_IN_THE_SUMMARY TRUE //if true go to graphics/new and change the IvsRankingIcon.png to you img.
-#define MON_ICON_ON_TRAINER_CARD TRUE //if true draw the party mon icons on the trainer card back.
+#define MON_ICON_ON_TRAINER_CARD TRUE //if true draw the party mons icon on the trainer card back.
 #define DISAPPEAR_HP_BAR_DURING_ANIMS TRUE //if true the hp bars disppears when a move anim is displayed.
 
 // need

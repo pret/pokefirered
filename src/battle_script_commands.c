3db6b86fc8b8b28b32f65a77042e06bfaddfc775
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-01-17 10:26:54 -0300

    Update battle_script_commands.c

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -1315,6 +1315,7 @@ static void atk04_critcalc(void)
         holdEffect = ItemId_GetHoldEffect(item, gBattlerAttacker, TRUE);
     gPotentialItemEffectBattler = gBattlerAttacker;
     critChance  = 2 * ((gBattleMons[gBattlerAttacker].status2 & STATUS2_FOCUS_ENERGY) != 0)
+	        + (gBattleMons[gBattlerAttacker].ability == ABILITY_SUPER_LUCK)
                 + (gBattleMoves[gCurrentMove].effect == EFFECT_HIGH_CRITICAL)
                 + (gBattleMoves[gCurrentMove].effect == EFFECT_SKY_ATTACK)
                 + (gBattleMoves[gCurrentMove].effect == EFFECT_BLAZE_KICK)

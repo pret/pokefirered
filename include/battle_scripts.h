e17457c8632f4dcbc11b99c8b138f317e462a78d
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-01-24 11:22:26 -0300

    Update battle_scripts.h

--- include/battle_scripts.h
+++ include/battle_scripts.h
@@ -247,6 +247,7 @@ extern const u8 BattleScript_DisplaySwitchInMsg[];
 extern const u8 BattleScript_MoveStatRaise[];
 extern const u8 BattleScript_MoveStatRaise_PPLoss[];
 extern const u8 BattleScript_SnowWarningActivates[];
+extern const u8 BattleScript_Pickpocket[];
 
 extern const u8 *const gBattleScriptsForMoveEffects[];
 extern const u8 *const gBattlescriptsForBallThrow[];

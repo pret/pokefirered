acf3cb841d51fe13fc39280f622197b8ab7918fa
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-01-26 16:26:07 -0300

    Update inserts.h

--- include/constants/inserts.h
+++ include/constants/inserts.h
@@ -8,8 +8,8 @@
 #define PARALYSIS_UPDATE TRUE //if true the speed drop of paralysis will be 50% instead of 75%.
 #define EFFECTIVENESS_ON_MENU TRUE //if true the move type name trade its color based on the move effectiveness in battle.
 #define NATURE_COLOURS TRUE //if true colours the stat in the summary screen depending on the nature.
-#define IVS_IN_THE_SUMMARY TRUE //if true go to graphics/new and change the IvsRankingIcon.png to you img.
-#define MON_ICON_ON_TRAINER_CARD TRUE //if true draw the party mons icon on the trainer card back.
+#define IVS_IN_THE_SUMMARY TRUE //if true go to graphics/interface and change the dex_pokeball,_types,_category_and_IVs_ranking.png to you liking.
+#define MON_ICON_ON_TRAINER_CARD TRUE //if true draw the party mon icons on the trainer card back.
 #define DISAPPEAR_HP_BAR_DURING_ANIMS TRUE //if true the hp bars disppears when a move anim is displayed.
 #define BADGE_BOOST TRUE //if true it applies the boosts given by each badge.
 

44f2e9b7ac73dfd7896a31dd9860ebe0fefdd399
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-01-27 19:17:17 -0300

    Update pokemon.c

--- src/pokemon.c
+++ src/pokemon.c
@@ -5046,20 +5046,13 @@ u16 GetEvolutionTargetSpecies(struct Pokemon *mon, u8 type, u16 evolutionItem)
                 if (friendship >= 220)
                     targetSpecies = gEvolutionTable[species][i].targetSpecies;
                 break;
-            // FR/LG removed the time of day evolutions due to having no RTC.
             case EVO_FRIENDSHIP_DAY:
-                /*
-                RtcCalcLocalTime();
-                if (gLocalTime.hours >= 12 && gLocalTime.hours < 24 && friendship >= 220)
+                if (gRtcLocation.hour >= DAWN_OF_DAY_START && gLocalTime.hours <= AFTERNOON_OF_DAY_START && friendship >= 220)
                     targetSpecies = gEvolutionTable[species][i].targetSpecies;
-                */
                 break;
             case EVO_FRIENDSHIP_NIGHT:
-                /*
-                RtcCalcLocalTime();
-                if (gLocalTime.hours >= 0 && gLocalTime.hours < 12 && friendship >= 220)
+                if ((gLocalTime.hours > AFTERNOON_OF_DAY_START || gLocalTime.hours < DAWN_OF_DAY_START) && friendship >= 220)
                     targetSpecies = gEvolutionTable[species][i].targetSpecies;
-                */
                 break;
             case EVO_LEVEL:
                 if (gEvolutionTable[species][i].param <= level)

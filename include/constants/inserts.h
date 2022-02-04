28d4a91e113ff6a18d863d15ea3acac68fd04e39
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-02-04 11:48:40 -0300

    Update inserts.h

--- include/constants/inserts.h
+++ include/constants/inserts.h
@@ -1,5 +1,3 @@
-#define MON_ICON_ON_ABILITY_POP_UP_BOX TRUE //if true the ability pop up go with the respective mon icon.
-#define LOAD_CUSTOM_ABILITY_POP_UP_IMG TRUE //if true go to graphics/new and change the abilitypopupbox.png to your box img, otherwise the box is the same of the Lv up box.
 #define EXP_BLOCK TRUE //if true go to src/battle_script_commands.c and edit the levels for each badge in sExpBlockLevels.
 #define PAGE_SWAP TRUE //if true auttomaticaly swap the naming screen page on write the first caracter.
 #define NO_HEAL_EGGS TRUE //if true the pokemon center animation don't coun't eggs.

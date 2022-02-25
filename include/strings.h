6720c7eb6989dfd68a90c7fbbb5f4dfafbf58624
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-02-25 19:28:20 -0300

    Update strings.h

--- include/strings.h
+++ include/strings.h
@@ -964,6 +964,7 @@ extern const u8 gText_InBagVar1[];
 extern const u8 gText_Var1AndYouWantedVar2[];
 extern const u8 gText_HereYouGoThankYou[];
 extern const u8 gText_NoMoreRoomForThis[];
+extern const u8 gText_YouAlreadyHaveThis[];
 
 // pokedex_screen
 extern const u8 gText_PokedexTableOfContents[];

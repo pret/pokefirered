280e582faec4e1c6d852d534b7a55ca40d6038d7
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-02-26 18:05:08 -0300

    Delete callasm_functions.h

--- include/constants/callasm_functions.h
+++
@@ -1,7 +0,0 @@
-#define DoAftermathDamage 0
-#define TryDoAnticipationShudder 1
-#define TryBadDreamsSecondDamage 2
-#define GetStatRaiseDownload 3
-#define GetStrongestMoveForewarn 4
-#define TryFriskFirstTarget 5
-#define TryFriskSecondTarget 6

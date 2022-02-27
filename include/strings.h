38faf35e695d7a090ad7587c35f5c47a674f59ab
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-02-27 18:18:13 -0300

    Update strings.h

--- include/strings.h
+++ include/strings.h
@@ -507,6 +507,7 @@ extern const u8 gText_ABtnRegistrationCounter[];
 // main_menu
 extern const u8 gText_SaveFileCorruptedPrevWillBeLoaded[];
 extern const u8 gText_SaveFileHasBeenDeleted[];
+extern const u8 gText_PreviousSaveFileWillBeLoaded[];
 extern const u8 gText_1MSubCircuitBoardNotInstalled[];
 extern const u8 gText_NewGame[];
 extern const u8 gText_Continue[];

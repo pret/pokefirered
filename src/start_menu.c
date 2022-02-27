f80145281e69eae865f77fa942457065fe01b540
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-02-27 18:36:02 -0300

    Update start_menu.c

--- src/start_menu.c
+++ src/start_menu.c
@@ -780,7 +780,11 @@ static u8 SaveDialogCB_PrintAskOverwriteText(void)
     if (gDifferentSaveFile == TRUE)
         PrintSaveTextWithFollowupFunc(gText_DifferentGameFile, SaveDialogCB_AskReplacePreviousFilePrintYesNoMenu);
     else
+#if SAVE_FIX
+        SaveDialogCB_PrintSavingDontTurnOffPower(); //unused return value
+#else
         PrintSaveTextWithFollowupFunc(gText_AlreadySaveFile_WouldLikeToOverwrite, SaveDialogCB_AskOverwritePrintYesNoMenu);
+#endif
     return SAVECB_RETURN_CONTINUE;
 }
 

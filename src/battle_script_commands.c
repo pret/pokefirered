64dc537c1282535fde153cc3d0030740a68f4096
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-02-27 17:07:01 -0300

    Update battle_script_commands.c

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -9245,7 +9245,7 @@ static void atkF9_cureprimarystatus(void)
 
 static void atkFA_setword(void)
 {
-	u32 *dest = T1_READ_PTR(gBattlescriptCurrInstr + 1);
+	u32 *dest = T2_READ_PTR(gBattlescriptCurrInstr + 1);
 	u32 word = T1_READ_32(gBattlescriptCurrInstr + 5);
 	*dest = word;
 	gBattlescriptCurrInstr += 9;

22fc16239cddf06afa7bf57dbc70cb64db1fa2a8
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-03-01 18:01:47 -0300

    Update inserts.h

--- include/constants/inserts.h
+++ include/constants/inserts.h
@@ -1,4 +1,4 @@
-#define EXP_BLOCK TRUE //if true go to src/battle_script_commands.c and edit the levels for each badge in sExpBlockLevels.
+#define EXP_BLOCK TRUE //if true it blocks the Exp gain based on the number of badges you has. Go to src/battle_script_commands.c and edit the levels for each badge in sExpBlockLevels.
 #define PAGE_SWAP TRUE //if true it auttomaticaly swap the naming screen page to lower case on write the first caracter.
 #define NO_HEAL_EGGS TRUE //if true the pokemon center animation don't count eggs.
 #define POISON_SURVIVAL TRUE //if true the pokemons don't die from poison in the overworld.
@@ -8,11 +8,11 @@
 #define CONFUSION_UPDATE TRUE //if true the chance that the pokemon will be damaged by confusion becames 33% instead of 50%.
 #define EFFECTIVENESS_ON_MENU TRUE //if true the move type name trade its color based on the move effectiveness in battle.
 #define NATURE_COLOURS TRUE //if true it colours the stat in the summary screen depending on the nature.
-#define IVS_IN_THE_SUMMARY TRUE //if true go to graphics/interface and change the dex_pokeball,_types,_category_and_IVs_ranking.png to you liking.
+#define IVS_IN_THE_SUMMARY TRUE //if true a raking based on your poke's Iv is showed in the summary screen. Go to graphics/interface and change the dex_pokeball,_types,_category_and_IVs_ranking.png to you liking.
 #define MON_ICON_ON_TRAINER_CARD TRUE //if true it draw the party mon icons on the trainer card back.
 #define DISAPPEAR_HP_BAR_DURING_ANIMS TRUE //if true the hp bars disppears when a move anim is displayed.
 #define BADGE_BOOST TRUE //if true it applies the boosts given by each badge.
-#define DONT_REPLENISH_MOVE_PP TRUE //if true when you overwrite a move with another with higher PP the actual PP will Remains.
+#define DONT_REPLENISH_MOVE_PP TRUE //if false when you overwrite a move with another with higher PP the actual PP will Remains.
 #define DELETABLE_HMS TRUE //if true all HM moves will be deleteable like any other TM.
 #define TIME_BOX_ON_START_MENU TRUE //if true it create a box that marks the actual time when open the start menu.
 #define REMOVE_FLASHBACKS TRUE //if true it removes the flashbacks when load a saved game.

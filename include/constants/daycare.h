063c375d53c19a1def9eb9c75cc8fa020006f7df
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-03-02 11:48:47 -0300

    Update daycare.h

--- include/constants/daycare.h
+++ include/constants/daycare.h
@@ -14,7 +14,7 @@
 #define DAYCARE_TWO_MONS    3
 
 #define INHERITED_IV_COUNT 3
-#define EGG_HATCH_LEVEL 5
+#define EGG_HATCH_LEVEL 1
 #define EGG_GENDER_MALE 0x8000  // used to create a male egg from a female-only parent species (e.g. Nidoran)
 
 #define DAYCARE_LEVEL_MENU_EXIT 5

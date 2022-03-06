0f2cfb963a44e7ad958123cf897ea469e2387664
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-03-06 10:26:50 -0300

    Update pokemon.c

--- src/pokemon.c
+++ src/pokemon.c
@@ -2324,6 +2324,32 @@ u16 MonTryLearningNewMove(struct Pokemon *mon, bool8 firstMove)
     return retVal;
 }
 
+u16 MonTryLearningNewMoveAfterEvolution(struct Pokemon *mon, bool8 firstMove)
+{
+	u16 moveLevel, species = GetMonData(mon, MON_DATA_SPECIES, NULL);
+	u8 level = GetMonData(mon, MON_DATA_LEVEL, NULL);
+	
+	// since you can learn more than one move per level
+	// the game needs to know whether you decided to
+	// learn it or keep the old set to avoid asking
+	// you to learn the same move over and over again
+	if (firstMove)
+		sLearningMoveTableID = 0;
+	while (gLevelUpLearnsets[species][sLearningMoveTableID] != LEVEL_UP_END)
+        {
+		moveLevel = (gLevelUpLearnsets[species][sLearningMoveTableID] & 0xFE00);
+		
+		while (moveLevel == 0 || moveLevel == (level << 9))
+		{
+			gMoveToLearn = (gLevelUpLearnsets[species][sLearningMoveTableID] & 0x1FF);
+			sLearningMoveTableID++;
+			return GiveMoveToMon(mon, gMoveToLearn);
+		}
+		sLearningMoveTableID++;
+        }
+	return 0;
+}
+
 void DeleteFirstMoveAndGiveMoveToMon(struct Pokemon *mon, u16 move)
 {
     s32 i;

c14ab0acd1f4a25d3465aba40d120ff0108417bc
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-03-13 08:56:44 -0300

    Update pokemon.c

--- src/pokemon.c
+++ src/pokemon.c
@@ -1828,7 +1828,7 @@ void CreateBoxMon(struct BoxPokemon *boxMon, u16 species, u8 level, u8 fixedIV, 
     SetBoxMonData(boxMon, MON_DATA_POKEBALL, &value);
     SetBoxMonData(boxMon, MON_DATA_OT_GENDER, &gSaveBlock2Ptr->playerGender);
 
-    if (fixedIV < USE_RANDOM_IVS)
+    if (fixedIV != USE_RANDOM_IVS)
     {
         SetBoxMonData(boxMon, MON_DATA_HP_IV, &fixedIV);
         SetBoxMonData(boxMon, MON_DATA_ATK_IV, &fixedIV);

220aeb67b3d30267716b16f989f600aad02ca69c
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-03-20 19:12:09 -0300

    Update start_menu.c

--- src/start_menu.c
+++ src/start_menu.c
@@ -125,16 +125,6 @@ static const struct MenuAction sStartMenuActionTable[] = {
     { gStartMenuText_Player, {.u8_void = StartMenuLinkPlayerCallback} }
 };
 
-static const struct WindowTemplate sTimeBoxWindowTemplate = {
-    .bg = 0,
-    .tilemapLeft = 1,
-    .tilemapTop = 1,
-    .width = 10,
-    .height = 2,
-    .paletteNum = 15,
-    .baseBlock = 0x008
-};
-
 static const u8 *const sStartMenuDescPointers[] = {
     gStartMenuDesc_Pokedex,
     gStartMenuDesc_Pokemon,

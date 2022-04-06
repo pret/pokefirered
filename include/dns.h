9d5e37fc04e8e9ddc9e84fb3e6b1d545c5d0233e
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-04-06 13:53:11 -0300

    Update dns.h

--- include/dns.h
+++ include/dns.h
@@ -6,19 +6,12 @@
 #define DNS_PAL_EXCEPTION   FALSE
 #define DNS_PAL_ACTIVE      TRUE
 
-struct LightingColour 
-{
-    u8 paletteNum;
-    u8 colourNum;
-    u16 lightColour;
-};
-
-struct DnsPalExceptions 
+struct DNSPalExceptions 
 {
     bool8 pal[32];
 };
 
-void DnsTransferPlttBuffer(void *src, void *dest);
-void DnsApplyFilters(void);
+void DNSTransferPlttBuffer(void *src, void *dest);
+void DNSApplyFilters(void);
 
 #endif // GUARD_DNS_UTILS_H

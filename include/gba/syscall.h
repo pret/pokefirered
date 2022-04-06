7d484aa6a6ae6927099c36a5762e1813b6c01d03
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-04-06 13:42:47 -0300

    Update syscall.h

--- include/gba/syscall.h
+++ include/gba/syscall.h
@@ -31,7 +31,6 @@ void CpuSet(const void *src, void *dest, u32 control);
 void CpuSet2(const void *src, u32 dest, u32 control);
 
 void RTCStart(struct RtcFill *Check1, struct RtcFuncStruct *PCTime, struct RtcStruct *GameTime);
-void DayAndNightPalleteChange(void *src, void *dest, u32 color);
 
 #define CPU_FAST_SET_SRC_FIXED 0x01000000
 

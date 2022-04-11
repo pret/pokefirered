7cd9a90a0cb8b9fbc04cde1637b54f67a780b49a
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-04-11 09:53:30 -0300

    Update trainer_slide.h

--- include/trainer_slide.h
+++ include/trainer_slide.h
@@ -1,6 +1,8 @@
 #ifndef GUARD_TRAINER_SLIDE_H
 #define GUARD_TRAINER_SLIDE_H
 
+#include "global.h"
+
 enum
 {
 	TRAINER_SLIDE_FIRST_DOWN,
@@ -8,6 +10,6 @@ enum
 	TRAINER_SLIDE_LAST_MON_LOW_HP,
 };
 
-bool8 ShouldDoTrainerSlide(u8 bank, u16 trainerId, u8 caseiD);
+bool8 ShouldDoTrainerSlide(u8 bank, u16 trainerId, u8 caseId);
 
 #endif // GUARD_TRAINER_SLIDE_H

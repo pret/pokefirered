349f5e23ed19673b72a81d6642e6d37f16a27c0a
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-04-12 12:21:36 -0300

    Update bag.h

--- include/bag.h
+++ include/bag.h
@@ -4,7 +4,6 @@
 #include "menu_helpers.h"
 
 void InitBagWindows(void);
-void BagPrintTextOnWindow(u8 windowId, u8 fontId, const u8 * str, u8 x, u8 y, u8 letterSpacing, u8 lineSpacing, u8 speed, u8 colorIdx);
 void BagPrintTextOnWin1CenteredColor0(const u8 * str, u8 unused);
 void BagDrawDepositItemTextBox(void);
 u8 ShowBagWindow(u8 whichWindow, u8 nItems);

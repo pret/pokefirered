3bf5f46e021d97fc5c40520d2aee373880e081f0
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-04-13 10:25:43 -0300

    Update dark.c

--- src/dark.c
+++ src/dark.c
@@ -785,7 +785,7 @@ void AnimTask_MetallicShine(u8 taskId)
     else
         species = GetMonData(&gPlayerParty[gBattlerPartyIndexes[gBattleAnimAttacker]], MON_DATA_SPECIES);
     spriteId = GetAnimBattlerSpriteId(ANIM_ATTACKER);
-    newSpriteId = CreateCloneOfSpriteInWindowMode(gBattleAnimAttacker, spriteId, species);
+    newSpriteId = CreateCloneOfSpriteInWindowMode(spriteId, species);
     GetBattleAnimBg1Data(&animBg);
     AnimLoadCompressedBgTilemap(animBg.bgId, gMetalShineTilemap);
     AnimLoadCompressedBgGfx(animBg.bgId, gMetalShineGfx, animBg.tilesOffset);

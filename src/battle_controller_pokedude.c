3d9db953ddecee4d451ecd44b59772a1f72d7f1b
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-04-13 17:55:10 -0300

    Update battle_controller_pokedude.c

--- src/battle_controller_pokedude.c
+++ src/battle_controller_pokedude.c
@@ -182,9 +182,6 @@ static void (*const sPokedudeBufferCommands[CONTROLLER_CMDS_COUNT])(void) =
         PokedudeCmdEnd,
     };
 
-// not used
-static const u8 gUnknown_8479000[] = { 0x48, 0x48, 0x20, 0x5a, 0x50, 0x50, 0x50, 0x58 };
-
 #define pdHealthboxPal1  simulatedInputState[0]
 #define pdHealthboxPal2  simulatedInputState[1]
 #define pdScriptNum      simulatedInputState[2]

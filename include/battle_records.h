bffdf6b965b3a2f5e177762bd702f13a21ccae8f
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-04-13 19:49:03 -0300

    Update battle_records.h

--- include/battle_records.h
+++ include/battle_records.h
@@ -1,6 +1,7 @@
 #ifndef GUARD_BATTLE_RECORDS_H
 #define GUARD_BATTLE_RECORDS_H
 
+void ClearPlayerLinkBattleRecords(void);
 void TryRecordLinkBattleOutcome(s32 battlerId);
 
 #endif // GUARD_BATTLE_RECORDS_H

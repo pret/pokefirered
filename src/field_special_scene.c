4398901f3f48bc99bf119fa1e9992e9468de77af
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-04-17 14:15:35 -0300

    Update field_special_scene.c

--- src/field_special_scene.c
+++ src/field_special_scene.c
@@ -1,26 +1,5 @@
 #include "global.h"
 
-static u32 sub_80A0E78(void)
-{
-    return 0;
-}
-
-static void nullsub_57(void)
-{
-}
-
-static void nullsub_58(void)
-{
-}
-
-static void nullsub_59(void)
-{
-}
-
-void nullsub_60(void)
-{
-}
-
 // From Hoenn's SS Tidal
 void LookThroughPorthole(void)
 {

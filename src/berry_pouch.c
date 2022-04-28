0b177251e5af49dafc8c0e14d6ae19334ed8814a
bugadorderoms <93982673+bugdorderoms@users.noreply.github.com>
2022-04-28 18:04:31 -0300

    Update berry_pouch.c

--- src/berry_pouch.c
+++ src/berry_pouch.c
@@ -707,7 +707,7 @@ static void BerryPouchMoveCursorFunc(s32 itemIndex, bool8 onInit, struct ListMen
     if (sResources->listMenuNumItems != itemIndex)
         sub_80989A0(BagGetItemIdByPocketPosition(POCKET_BERRY_POUCH, itemIndex), sResources->itemMenuIconId);
     else
-        sub_80989A0(ITEM_N_A, sResources->itemMenuIconId);
+        sub_80989A0(ITEMS_COUNT, sResources->itemMenuIconId);
     sResources->itemMenuIconId ^= 1;
     PrintSelectedBerryDescription(itemIndex);
 }

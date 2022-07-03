951b72145f9d68d7c2638b4d14c1c337731da502
cbt6 <91667135+cbt6@users.noreply.github.com>
2022-07-03 23:48:40 +0800

    Document src/field_message_box.c

--- include/field_message_box.h
+++ include/field_message_box.h
@@ -7,7 +7,7 @@ bool8 ShowFieldMessage(const u8 *message);
 bool8 ShowFieldAutoScrollMessage(const u8 *message);
 void HideFieldMessageBox(void);
 bool8 IsFieldMessageBoxHidden(void);
-bool8 textbox_any_visible(void);
+u8 GetFieldMessageBoxType(void);
 void InitFieldMessageBox(void);
 
 #endif // GUARD_FIELD_MESSAGE_BOX_H

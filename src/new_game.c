b1fc7d42b1432f52deca53f6bc9fe9cc41fe1620
Alexander Dignan <alexander.dignan@mrisoftware.com>
2023-06-07 23:49:30 -0300

    don't ask for name in dialog

--- src/new_game.c
+++ src/new_game.c
@@ -59,7 +59,7 @@ static void InitPlayerTrainerId(void)
 
 static void SetDefaultOptions(void)
 {
-    gSaveBlock2Ptr->optionsTextSpeed = OPTIONS_TEXT_SPEED_MID;
+    gSaveBlock2Ptr->optionsTextSpeed = OPTIONS_TEXT_SPEED_FAST;
     gSaveBlock2Ptr->optionsWindowFrameType = 0;
     gSaveBlock2Ptr->optionsSound = OPTIONS_SOUND_MONO;
     gSaveBlock2Ptr->optionsBattleStyle = OPTIONS_BATTLE_STYLE_SHIFT;

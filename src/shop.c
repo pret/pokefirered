cc1c4c3a4df54738e7b51cb184768d0d1f0e9733
FireRune <46537748+FireRune@users.noreply.github.com>
2024-02-24 17:14:16 +0100

    update shop.c

--- src/shop.c
+++ src/shop.c
@@ -896,6 +896,10 @@ static void Task_BuyMenu(u8 taskId)
             {
                 BuyMenuDisplayMessage(taskId, gText_YouDontHaveMoney, BuyMenuReturnToItemList);
             }
+            else if (ItemId_GetPocket(itemId) == POCKET_TM_CASE && CheckBagHasItem(itemId, 1))
+            {
+                BuyMenuDisplayMessage(taskId, gText_YouAlreadyHaveThis, BuyMenuReturnToItemList);
+            }
             else
             {
                 CopyItemName(itemId, gStringVar1);

8f8c769cc3ee10c47a8d5d5c6c565d62a2bbe9b6
FireRune <46537748+FireRune@users.noreply.github.com>
2024-02-24 17:14:43 +0100

    update item_icon_table.h

--- src/data/item_icon_table.h
+++ src/data/item_icon_table.h
@@ -398,6 +398,8 @@ static const u32 *const sItemIconTable[ITEMS_COUNT + 1][2] =
     [ITEM_POWDER_JAR]     = {gItemIcon_PowderJar, gItemIconPalette_PowderJar},
     [ITEM_RUBY]           = {gItemIcon_Gem, gItemIconPalette_Ruby},
     [ITEM_SAPPHIRE]       = {gItemIcon_Gem, gItemIconPalette_Sapphire},
+    // new items for frlg_combined
+    [ITEM_LINKING_CORD]   = {gItemIcon_LinkingCord, gItemIconPalette_LinkingCord},
     // Return to field arrow
     [ITEMS_COUNT]         = {gItemIcon_ReturnToFieldArrow, gItemIconPalette_ReturnToFieldArrow},
 };

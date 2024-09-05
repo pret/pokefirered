7aa0c9b46eee511376238e9caeb4c0f17d6f14e0
cawtds <38510667+cawtds@users.noreply.github.com>
2024-09-05 16:19:04 +0200

    remove buy_menu_helpers

--- include/shop.h
+++ include/shop.h
@@ -12,13 +12,4 @@ void CreateDecorationShop2Menu(const u16 *);
 u8 GetMartFontId(void);
 void RecordItemTransaction(u16 itemId, u16 quantity, u8 logEventId);
 
-// buy_menu_helper
-void BuyMenuInitWindows(bool32 isSellingTM);
-void BuyMenuDrawMoneyBox(void);
-void BuyMenuPrint(u8 windowId, u8 font, const u8 *text, u8 x, u8 y, u8 letterSpacing, u8 lineSpacing, u8 speed, u8 color);
-void BuyMenuDisplayMessage(u8 taskId, const u8 *text, TaskFunc callback);
-void BuyMenuQuantityBoxNormalBorder(u8 windowId, bool8 copyToVram);
-void BuyMenuQuantityBoxThinBorder(u8 windowId, bool8 copyToVram);
-void BuyMenuConfirmPurchase(u8 taskId, const struct YesNoFuncTable *yesNo);
-
 #endif // GUARD_SHOP_H

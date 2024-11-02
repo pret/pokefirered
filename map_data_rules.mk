d1a7c6c74f14c697d5df915bef9a216aca8f0a96
cawtds <38510667+cawtds@users.noreply.github.com>
2024-11-02 14:27:51 +0100

    makefile cleanup

--- map_data_rules.mk
+++ map_data_rules.mk
@@ -32,32 +32,3 @@ $(MAPS_OUTDIR)/connections.inc $(MAPS_OUTDIR)/groups.inc $(MAPS_OUTDIR)/events.i
 
 $(LAYOUTS_OUTDIR)/layouts.inc $(LAYOUTS_OUTDIR)/layouts_table.inc $(INCLUDECONSTS_OUTDIR)/layouts.h: $(LAYOUTS_DIR)/layouts.json
 	$(MAPJSON) layouts firered $< $(LAYOUTS_OUTDIR) $(INCLUDECONSTS_OUTDIR)
-
-
-
-
-
-# old:
-# $(MAPS_DIR)/%/header.inc: $(MAPS_DIR)/%/map.json
-# 	$(MAPJSON) map firered $< $(LAYOUTS_DIR)/layouts.json
-# $(MAPS_DIR)/%/events.inc: $(MAPS_DIR)/%/header.inc ;
-# $(MAPS_DIR)/%/connections.inc: $(MAPS_DIR)/%/events.inc ;
-
-# $(MAPS_DIR)/groups.inc: $(MAPS_DIR)/map_groups.json
-# 	$(MAPJSON) groups firered $<
-# $(MAPS_DIR)/connections.inc: $(MAPS_DIR)/groups.inc ;
-# $(MAPS_DIR)/events.inc: $(MAPS_DIR)/connections.inc ;
-# $(MAPS_DIR)/headers.inc: $(MAPS_DIR)/events.inc ;
-# $(DATA_SRC_SUBDIR)/map_group_count.h: $(MAPS_DIR)/headers.inc ;
-# include/constants/map_groups.h: $(MAPS_DIR)/headers.inc ;
-
-# $(LAYOUTS_DIR)/layouts.inc: $(LAYOUTS_DIR)/layouts.json
-# 	$(MAPJSON) layouts firered $<
-# $(LAYOUTS_DIR)/layouts_table.inc: $(LAYOUTS_DIR)/layouts.inc ;
-# include/constants/layouts.h: $(LAYOUTS_DIR)/layouts_table.inc ;
-
-# $(DATA_ASM_BUILDDIR)/maps.o: $(DATA_ASM_SUBDIR)/maps.s $(LAYOUTS_DIR)/layouts.inc $(LAYOUTS_DIR)/layouts_table.inc $(MAPS_DIR)/headers.inc $(MAPS_DIR)/groups.inc $(MAPS_DIR)/connections.inc $(MAP_CONNECTIONS) $(MAP_HEADERS)
-# 	$(PREPROC) $< charmap.txt | $(CPP) -I include -nostdinc -undef -Wno-unicode - | $(AS) $(ASFLAGS) -o $@
-# $(DATA_ASM_BUILDDIR)/map_events.o: $(DATA_ASM_SUBDIR)/map_events.s $(MAPS_DIR)/events.inc $(MAP_EVENTS)
-# 	$(PREPROC) $< charmap.txt | $(CPP) -I include -nostdinc -undef -Wno-unicode - | $(AS) $(ASFLAGS) -o $@
-

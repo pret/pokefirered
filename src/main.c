4b8004d77c18d18f393090d058a1b4fcca165b9b
cawtds <38510667+cawtds@users.noreply.github.com>
2025-01-18 12:31:32 +0100

    reenable help system

--- src/main.c
+++ src/main.c
@@ -185,12 +185,11 @@ static void InitMainCallbacks(void)
 
 static void CallCallbacks(void)
 {
-// #if TESTING || DEBUG_BATTLE_MENU == TRUE // test framework not working with help system
-//     if (!RunSaveFailedScreen())
-// #else
-//     if (!RunSaveFailedScreen() && !RunHelpSystemCallback())
-// #endif
+#if TESTING || DEBUG_BATTLE_MENU == TRUE // test framework not working with help system
     if (!RunSaveFailedScreen())
+#else
+    if (!RunSaveFailedScreen() && !RunHelpSystemCallback())
+#endif
     {
         if (gMain.callback1)
             gMain.callback1();

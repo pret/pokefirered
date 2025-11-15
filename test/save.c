32bf4db61dcfa124aea38641e9db50294c0972b4
cawtds <38510667+cawtds@users.noreply.github.com>
2025-11-15 03:22:08 +0100

    fix save test

--- test/save.c
+++ test/save.c
@@ -4,7 +4,7 @@
 
 // If you would like to ensure save compatibility, update the values below with those for your hack. You can find these through the debug menu.
 // Please note that this simple check is not 100% foolproof, but should be able to catch most unintended shifts.
-#define T_SAVEBLOCK1_SIZE 15808
+#define T_SAVEBLOCK1_SIZE 15812
 #define T_SAVEBLOCK2_SIZE 3968
 #define T_SAVEBLOCK3_SIZE 4
 #define T_POKEMONSTORAGE_SIZE 34144

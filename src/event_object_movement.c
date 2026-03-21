2b52240b37f0137dfe17de4b5e0edfc1673a5509
AZero13 <83477269+SiliconA-Z@users.noreply.github.com>
2026-03-21 18:57:22 -0400

    Have GetAvailableObjectEventId match in all versions
    agbcc probably was affected by whatever assert was in whatever file in here and generated different code.

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -1495,30 +1495,19 @@ static bool8 GetAvailableObjectEventId(u16 localId, u8 mapNum, u8 mapGroup, u8 *
 {
     u8 i = 0;
 
-#if REVISION >= 0xA
     for (i = 0; i < OBJECT_EVENTS_COUNT && gObjectEvents[i].active; i++)
-#else
-    for (i = 0; i < OBJECT_EVENTS_COUNT; i++)
-#endif
     {
-
-#if REVISION >= 0xA
-#else
-        if (!gObjectEvents[i].active)
-            break;
-#endif
         if (gObjectEvents[i].localId == localId && gObjectEvents[i].mapNum == mapNum && gObjectEvents[i].mapGroup == mapGroup)
             return TRUE;
     }
     if (i >= OBJECT_EVENTS_COUNT)
         return TRUE;
     *objectEventId = i;
-    do
+    for (; i < OBJECT_EVENTS_COUNT; i++)
     {
         if (gObjectEvents[i].active && gObjectEvents[i].localId == localId && gObjectEvents[i].mapNum == mapNum && gObjectEvents[i].mapGroup == mapGroup)
             return TRUE;
-        i++;
-    } while (i < OBJECT_EVENTS_COUNT);
+    }
     return FALSE;
 }
 

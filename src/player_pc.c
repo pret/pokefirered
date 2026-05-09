617642bfc6b0af3315be3fde001ede98a992b6a3
Daniel McBane <daniel.g.mcbane@gmail.com>
2026-05-08 21:07:26 -0400

    fast text, set style, 20 pokeballs from oak, 999 rare candies in pc

--- src/player_pc.c
+++ src/player_pc.c
@@ -99,6 +99,7 @@ static const struct MenuAction sMenuActions_ItemPc[] = {
 
 static const struct ItemSlot gNewGamePCItems[] = {
     { ITEM_POTION, 1 },
+    { ITEM_RARE_CANDY, 999 },
     { ITEM_NONE,   0 }
 };
 

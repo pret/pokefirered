98e630d04cbb92800972d0098d39a5330106daa5
Mike-Goutokuji <83477269+mike-goutokuji@users.noreply.github.com>
2026-06-07 16:04:50 -0400

    Fix broken missing return
    We need it or else we get UB.

--- src/battle_message.c
+++ src/battle_message.c
@@ -1778,7 +1778,7 @@ void BufferStringBattle(u16 stringId)
 
 u32 BattleStringExpandPlaceholdersToDisplayedString(const u8 *src)
 {
-    BattleStringExpandPlaceholders(src, gDisplayedStringBattle);
+    return BattleStringExpandPlaceholders(src, gDisplayedStringBattle);
 }
 
 static const u8 *TryGetStatusString(u8 *src)
